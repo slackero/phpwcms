3e3f332e28ad0c2da57b5c68c500fb1158d25e08
slackero <slackero@1c448b33-d038-0410-9355-190e454d89af>
2008-02-28 09:31:21 +0000

    Backend "is logged-in" check replaced by internal function than include php file. This is bit faster and can be better controlled like needed for Ajax actions where no simple redirect is possible.

--- include/inc_tmpl/content/cnt51.open.php
+++ include/inc_tmpl/content/cnt51.open.php
@@ -27,8 +27,8 @@ require_once('../../../config/phpwcms/conf.inc.php');
 require_once('../../../include/inc_lib/default.inc.php');
 require_once (PHPWCMS_ROOT.'/include/inc_lib/dbcon.inc.php');
 require_once (PHPWCMS_ROOT.'/include/inc_lib/general.inc.php');
+checkLogin();
 require_once (PHPWCMS_ROOT.'/include/inc_lib/backend.functions.inc.php');
-require_once (PHPWCMS_ROOT.'/include/inc_lib/checklogin.inc.php');
 
 
 ?>
