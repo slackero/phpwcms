cd04015b59af157825d4136d98b3f2147668f6a3
slackero <slackero@1c448b33-d038-0410-9355-190e454d89af>
2008-02-28 09:18:23 +0000

    multiple file upload for file center implemented using Mootools/Flash based FancyUpload, it's a 2-step upload process where files are first uploaded to ftp directory and then taken over using "ftp takeover"

---
+++ include/inc_tmpl/files.multipleupload.tmpl.php
@@ -0,0 +1,77 @@
+<?php
+/*************************************************************************************
+   Copyright notice
+   
+   (c) 2002-2008 Oliver Georgi (oliver@phpwcms.de) // All rights reserved.
+ 
+   This script is part of PHPWCMS. The PHPWCMS web content management system is
+   free software; you can redistribute it and/or modify it under the terms of
+   the GNU General Public License as published by the Free Software Foundation;
+   either version 2 of the License, or (at your option) any later version.
+  
+   The GNU General Public License can be found at http://www.gnu.org/copyleft/gpl.html
+   A copy is found in the textfile GPL.txt and important notices to the license 
+   from the author is found in LICENSE.txt distributed with these scripts.
+  
+   This script is distributed in the hope that it will be useful, but WITHOUT ANY 
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+   This copyright notice MUST APPEAR in all copies of the script!
+*************************************************************************************/
+
+// ----------------------------------------------------------------
+// obligate check for phpwcms constants
+if (!defined('PHPWCMS_ROOT')) {
+   die("You Cannot Access This Script Directly, Have a Nice Day.");
+}
+// ----------------------------------------------------------------
+
+
+// initialize Mootools
+initMultipleUpload();
+
+?>
+<h1 class="title" id="swiffStore"><?php echo $BL['be_file_multiple_upload'] ?></h1>
+
+<form action="<?=PHPWCMS_URL?>include/inc_act/ajax_uploader.php?<?= session_name().'='.session_id(); ?>" method="post" id="upload">
+
+<table border="0" cellpadding="0" cellspacing="0" summary="" class="width440 tdbottom10">
+<tr>
+	<td>
+	<ul class="upload-queue" id="upload-queue">
+		<li style="display: none">&nbsp;</li>
+	</ul>
+	</td>
+	</tr>
+</table>
+
+<div class="upload_button">
+	<input type="file" name="Filedata" id="upload-filedata" />
+	&nbsp;&nbsp;
+	<input name="submit" type="submit" class="v12" id="profile-submit" value="2) <?php echo $BL['be_files_upload'] ?>" />
+</div>
+
+</form>
+
+<script type="text/javascript">
+<!--
+
+window.addEvent('domready', function(){
+
+	var upload = new FancyUpload( $('upload-filedata'), {
+		swf: '<?= PHPWCMS_URL ?>include/inc_js/mootools/FancyUpload/Swiff.Uploader.swf',
+		queueList: 'upload-queue',
+		container: $('swiffStore'),
+		types: {
+			'<?=$BL['be_ctype_images']?> (*.jpg, *.jpeg, *.gif, *.png)': '*.jpg; *.jpeg; *.gif; *.png;',
+			'<?=$BL['be_ftptakeover_all']?> (*.*)': '*.*;'
+			},
+		limitSize: <?=$phpwcms['file_maxsize']?>,
+		browseButtonText: '1) <?=$BL['be_files_browse']?>'
+	});
+
+});
+
+//-->
+</script>
