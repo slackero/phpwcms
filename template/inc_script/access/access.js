4782e6d46ca9ee24fae47d63e6030d469399f417
slackero <slackero@1c448b33-d038-0410-9355-190e454d89af>
2008-03-07 18:11:18 +0000

    simple script which allows to wrap special content sections and display a "blocking" dialog where user has to agree to be able to get full access to content.

---
+++ template/inc_script/access/access.js
@@ -0,0 +1,59 @@
+window.addEvent('domready', function() {
+	
+	
+	if(Cookie.get('phpwcmsAgree') == '1') {
+	
+		hasAgreed();
+		
+	} else {
+		
+		
+		var overlay = new Element('div', {'id': 'accessOverlay'}).injectInside(document.body);
+		$('access_dialog').addClass('accessDialog');
+		
+		overlay.setStyles({'top': window.getScrollTop(), 'height': window.getHeight(), 'opacity': .75});
+		window.addEvent('resize', function(){ overlay.setStyles({'top': window.getScrollTop(), 'height': window.getHeight()}); });
+		$('access_save').setStyle('display','');
+	
+		var agree_checkbox = $('access_agree');
+		agree_checkbox.rel = 0;
+										 
+		$('agree_button_reject').addEvent('click', function(){agree_checkbox.rel = 1} );
+		$('agree_button_agree').addEvent('click', function(){agree_checkbox.rel = 2} );
+										 
+		$('access_form').addEvent('submit', function(r) {
+													 
+			
+			var r = new Event(r).stop();
+													 
+			if(agree_checkbox.rel == 1) {
+	
+				Cookie.remove('phpwcmsAgree');
+				document.location.href = redirect;
+			
+			} else if(agree_checkbox.rel == 2 && agree_checkbox.checked==false) {
+			
+				alert(erroralert);
+			
+			} else {
+			
+				Cookie.set('phpwcmsAgree', '1', {duration: 0, path: '/'});
+				window.removeEvent('resize');
+				hasAgreed();
+				overlay.remove();
+				
+			}
+			
+		});
+	
+	}
+
+});
+
+
+function hasAgreed() {
+	
+	$('access_dialog').setStyle('display','none');
+	$('access_save').setStyle('display','');
+
+}
---
+++ template/inc_script/frontend_render/disabled/access_dialog.php
@@ -0,0 +1,89 @@
+<?php
+
+$ACCESS					= array();
+
+$ACCESS['prefix']		= '<!-- Access //-->';
+$ACCESS['suffix']		= '<!-- Access Close //-->';
+
+$ACCESS['error']		= false;
+$ACCESS['error_js']		= 'You have to agree.';
+
+// file which contains dialog info located under 'template/inc_script/access/'
+$ACCESS['source_data']	= 'access_template.html';
+
+$redirect_id	= $content['struct'][ $content['cat_id'] ]['acat_struct'];
+$redirect		= empty($content['struct'][ $redirect_id ]['acat_alias']) ? '?id=' . $redirect_id : '?' . $content['struct'][ $redirect_id ]['acat_alias'];
+$redirect		= PHPWCMS_URL.'index.php'.$redirect;
+
+if(!empty($_SERVER['HTTP_REFERER']) && strpos(strtolower($_SERVER['HTTP_REFERER']), strtolower(PHPWCMS_URL)) !== FALSE) {
+	$redirect = $_SERVER['HTTP_REFERER'];
+}
+
+if(isset($_POST['agree_reject'])) {
+
+	if(isset($_SESSION['phpwcmsAgree'])) {
+		unset($_SESSION['phpwcmsAgree']);
+	}
+	setcookie('phpwcmsAgree', '0', time()-1000000, '/');
+	
+	if(isset($_POST['agree_redirect'])) {
+		$redirect = clean_slweg($_POST['agree_redirect']);
+	}
+	
+	headerRedirect($redirect);
+
+} elseif(isset($_POST['agree_agree'])) {
+
+	if(empty($_POST['access_agree']) || $_POST['access_agree'] != 'agree') {
+	
+		$ACCESS['error'] = true;
+	
+	} else {
+	
+		setcookie('phpwcmsAgree', '1', 0, '/');
+		$_SESSION['phpwcmsAgree'] = true;
+
+	}
+
+}
+
+if(!empty($_SESSION['phpwcmsAgree']) || ( isset($_COOKIE['phpwcmsAgree']) && $_COOKIE['phpwcmsAgree'] == 1)) {
+
+	$content['all'] = str_replace($ACCESS['prefix'], '', $content['all']);
+	$content['all'] = str_replace($ACCESS['suffix'], '', $content['all']);
+
+} elseif( strpos($content['all'], $ACCESS['prefix']) !== FALSE && strpos($content['all'], $ACCESS['suffix']) !== FALSE ) {
+
+	$block['custom_htmlhead']['mootools.js'] = '  <script src="'.TEMPLATE_PATH.'inc_js/mootools/mootools.js" type="text/javascript"></script>';
+	$block['custom_htmlhead']['access.js']   = '  <script src="'.TEMPLATE_PATH.'inc_script/access/access.js" type="text/javascript"></script>';
+	$block['custom_htmlhead']['set_vars']    = '  <script type="text/javascript">'.LF.'  <!--'.LF;
+	$block['custom_htmlhead']['set_vars']   .= '  var redirect="'.$redirect.'";' . LF;
+	$block['custom_htmlhead']['set_vars']   .= '  var erroralert="'.$ACCESS['error_js'].'";';
+	$block['custom_htmlhead']['set_vars']   .= LF.'  //-->'.LF.'  </script>';
+	$block['custom_htmlhead']['access.css']  = '  <link rel="stylesheet" type="text/css" href="'.TEMPLATE_PATH.'inc_script/access/access.css" />';
+	
+	$ACCESS['source_data'] = @file_get_contents(PHPWCMS_TEMPLATE.'inc_script/access/'.$ACCESS['source_data']);
+	
+	if($ACCESS['source_data']) {
+	
+		$ACCESS['source_data']  = render_cnt_template($ACCESS['source_data'], 'ERROR', $ACCESS['error'] ? '-' : '');
+	
+		$ACCESS['source_data']  = str_replace('{CURRENT_URL}', FE_CURRENT_URL, $ACCESS['source_data']);
+		$ACCESS['source_data']  = str_replace('{REDIRECT}', html_specialchars($redirect), $ACCESS['source_data']);
+
+		$ACCESS['dialog']  = '<div id="access_dialog">' . LF . $ACCESS['source_data'] . LF . '</div>' . LF ;
+		$ACCESS['dialog'] .= '<div id="access_save" style="display:none">';
+		
+		$content['all'] = str_replace($ACCESS['prefix'], $ACCESS['dialog'], $content['all']);
+		$content['all'] = str_replace($ACCESS['suffix'], LF . '</div>', $content['all']);
+
+	}
+
+} else {
+
+	$content['all'] = str_replace($ACCESS['prefix'], '', $content['all']);
+	$content['all'] = str_replace($ACCESS['suffix'], '', $content['all']);
+	
+}
+
+?>
