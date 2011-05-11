96dba910c29fec4066d004c77cfcb9e5e08bb2c4
slackero <slackero@1c448b33-d038-0410-9355-190e454d89af>
2011-05-11 10:59:58 +0000

    Fixed revision and date

--- setup/inc/setup.func.inc.php
+++ setup/inc/setup.func.inc.php
@@ -22,8 +22,8 @@
 
 //setup functions
 $phpwcms_version		= '1.4.7';
-$phpwcms_release_date	= '2011/05/05';
-$phpwcms_revision		= '417';
+$phpwcms_release_date	= '2011/05/11';
+$phpwcms_revision		= '419';
 
 function read_textfile($filename) {
 	if(is_file($filename)) {
