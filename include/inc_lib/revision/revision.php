5d0b27d0b96d8baa94d2dff380332554690e5aed
slackero <slackero@1c448b33-d038-0410-9355-190e454d89af>
2012-04-07 18:15:17 +0000

    Better check against remote JS files

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -21,7 +21,7 @@
 *************************************************************************************/
 
 define('PHPWCMS_VERSION',		'1.5');
-define('PHPWCMS_RELEASE_DATE',	'2012/04/07');
-define('PHPWCMS_REVISION',		'461');
+define('PHPWCMS_RELEASE_DATE',	'2012/04/08');
+define('PHPWCMS_REVISION',		'462');
 
 ?>
