6e2a582ea540c882e070f2a28bc3f8242693d6f7
Oliver Georgi <slackero+github@gmail.com>
2013-02-15 07:23:38 +0100

    Issue #16

---
+++ include/inc_lib/revision/r529.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * phpwcms content management system
+ *
+ * @author Oliver Georgi <oliver@phpwcms.de>
+ * @copyright Copyright (c) 2002-2013, Oliver Georgi
+ * @license http://opensource.org/licenses/GPL-2.0 GNU GPL-2
+ * @link http://www.phpwcms.de
+ *
+ **/
+
+
+// Revision 529 Update Check
+function phpwcms_revision_r529() {
+		
+	$status = true;
+	
+	// do former revision check – fallback to r528
+	if(phpwcms_revision_check_temp('528') !== true) {
+		$status = phpwcms_revision_check('528');
+	}
+	
+	// reset article and file manager status (open/close)
+	_dbUpdate('phpwcms_user', array(
+		'usr_var_structure' => '',
+		'usr_var_publicfile' => '',
+		'usr_var_privatefile' => ''
+	));
+	
+	return $status;
+}
+
+?>
