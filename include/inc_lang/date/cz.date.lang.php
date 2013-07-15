4bf75f62bb2fc002af4a6fe2b8cdac67b72a71e4
Oliver Georgi <slackero+github@gmail.com>
2013-07-15 07:21:14 +0200

    Czech language code cz added (in addition to older cs)

---
+++ include/inc_lang/date/cz.date.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * phpwcms content management system
+ *
+ * @author Oliver Georgi <oliver@phpwcms.de>
+ * @copyright Copyright (c) 2002-2013, Oliver Georgi
+ * @license http://opensource.org/licenses/GPL-2.0 GNU GPL-2
+ * @link http://www.phpwcms.de
+ *
+ **/
+
+// cz.date.lang.php
+// rev 1.0; 06-20-2004
+
+// language: cz
+
+$weekday_long[0]	= "Nedìle";		    $weekday_short[0]	= "Ne";
+$weekday_long[1]	= "Pondìlí";			$weekday_short[1]	= "Po";
+$weekday_long[2]	= "Úterý";		    $weekday_short[2]	= "Út";
+$weekday_long[3]	= "Støeda";		    $weekday_short[3]	= "St";
+$weekday_long[4]	= "Ètvrtek";		  $weekday_short[4]	= "Èt";
+$weekday_long[5]	= "Pátek";		    $weekday_short[5]	= "Pá";
+$weekday_long[6]	= "Sobota";		    $weekday_short[6]	= "So";
+
+$month_long[ 1]	= "Leden";			$month_short[ 1]		= "Led.";
+$month_long[ 2]	= "Únor";		    $month_short[ 2]		= "Úno.";
+$month_long[ 3]	= "Bøezen";			$month_short[ 3]		= "Bøe.";
+$month_long[ 4]	= "Duben";			$month_short[ 4]		= "Dub.";
+$month_long[ 5]	= "Kvìten";			$month_short[ 5]		= "Kvì.";
+$month_long[ 6]	= "Èerven";			$month_short[ 6]		= "Èer.";
+$month_long[ 7]	= "Èervenec";		$month_short[ 7]		= "Èen.";
+$month_long[ 8]	= "Srpen";			$month_short[ 8]		= "Srp.";
+$month_long[ 9]	= "Záøí";		    $month_short[ 9]		= "Záø.";
+$month_long[10]	= "Øíjen";		  $month_short[10]		= "Øíj.";
+$month_long[11]	= "Listopad";		$month_short[11]		= "Lis.";
+$month_long[12]	= "Prosinec";		$month_short[12]		= "Pro.";
+
+?>
