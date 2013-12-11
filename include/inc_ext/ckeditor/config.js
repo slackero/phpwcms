eedaadecc7561fa5db8fc18a376bf7d218d8b7c1
Oliver Georgi <slackero+github@gmail.com>
2013-12-11 15:56:25 +0100

    CKEditor 4.3.1

--- include/inc_ext/ckeditor/config.js
+++ include/inc_ext/ckeditor/config.js
@@ -1,10 +1,13 @@
 /**
  * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
