55b052940ca3059716e26526831b264a905c5926
Oliver Georgi <slackero+github@gmail.com>
2014-01-21 11:02:25 +0100

    Issue #59 — update alias of non deleted articles only

--- setup/upgrade_articlealias.php
+++ setup/upgrade_articlealias.php
@@ -19,28 +19,27 @@ require_once (PHPWCMS_ROOT.'/include/inc_lib/dbcon.inc.php');
 require_once (PHPWCMS_ROOT.'/include/inc_lib/general.inc.php');
 require_once (PHPWCMS_ROOT.'/include/inc_lib/backend.functions.inc.php');
 
-?>
-<html>
+?><html>
 <body>
 <h3>Upgrade article alias</h3>
 <?php
 
 // get all articles
-if($all = _dbQuery("SELECT article_id, article_alias, article_title FROM ".DB_PREPEND."phpwcms_article")) {
+if($all = _dbQuery("SELECT article_id, article_alias, article_title FROM ".DB_PREPEND."phpwcms_article WHERE article_deleted != 9")) {
 
 	foreach($all as $key => $value) {
 
 		$alias = empty($value['article_alias']) ? $value['article_title'] : $value['article_alias'];
 		$alias = proof_alias($value['article_id'], $alias, 'ARTICLE');
-		
+
 		if($alias != $value['article_alias']) {
-		
+
 			$sql  = "UPDATE ".DB_PREPEND."phpwcms_article SET ";
 			$sql .= "article_alias = '".aporeplace($alias)."' ";
 			$sql .= "WHERE article_id = ".$value['article_id']." LIMIT 1";
-			
+
 			$result = _dbQuery($sql, 'UPDATE');
-			
+
 			echo '<pre';
 			if($result === false) {
 				echo ' style="color:#CC3300"';
