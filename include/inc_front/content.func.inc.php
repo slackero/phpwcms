11c2e3533b8a18b2dc71d33c1555c2b247571352
Oliver Georgi <slackero+github@gmail.com>
2014-02-14 07:54:47 +0100

    Do not allow preview mode if backend login is no longer valid

--- include/inc_front/content.func.inc.php
+++ include/inc_front/content.func.inc.php
@@ -37,10 +37,17 @@ $PERMIT_ACCESS					= true; // by default set all content without permissions
 $CUSTOM							= array(); // var that holds result of content part "php var"
 
 // reset force redirect in case it is a POST
-if(empty($_POST) || isset($_GET['phpwcms-preview'])) {
+if(!empty($_POST)) {
+	$phpwcms['force301_id2alias']	= false;
+	$phpwcms['force301_2struct']	= false;
+} elseif(isset($_GET['phpwcms-preview'])) {
 	$phpwcms['force301_id2alias']	= false;
 	$phpwcms['force301_2struct']	= false;
 	unset($_GET['phpwcms-preview'], $_getVar['phpwcms-preview']);
+	checkLoginCount();
+	if(empty($_SESSION["wcs_user"])) {
+		headerRedirect(abs_url());
+	}
 }
 
 //method to get the right action values
