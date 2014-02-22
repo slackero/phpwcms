3669b66b52b9ddb2d0e0f7508b943ff701497045
Oliver Georgi <slackero+github@gmail.com>
2014-02-22 11:56:52 +0100

    Implement Open Graph rendering

--- include/inc_front/content.func.inc.php
+++ include/inc_front/content.func.inc.php
@@ -994,6 +994,42 @@ if(empty($block['custom_htmlhead']['meta.keywords']) && !empty($content['all_key
 		set_meta('keywords', implode(', ', $content['all_keywords']));
 	}
 }
+
+// Built-in Open Graph rendering
+if($content['opengraph']['support']) {
+	set_meta('og:type', $content['opengraph']['type'], 'property');
+	set_meta('og:title', sanitize_replacement_tags($content['opengraph']['title']), 'property');
+	if(empty($content['opengraph']['url'])) {
+		set_meta('og:url', abs_url(array(), array('phpwcms_output_action', 'print', 'phpwcms-preview', 'unsubscribe', 'subscribe')), 'property');
+	} else {
+		set_meta('og:url', $content['opengraph']['url'], 'property');
+	}
+
+	if(!empty($content['opengraph']['description'])) {
+		set_meta('og:description', sanitize_replacement_tags($content['opengraph']['description']), 'property');
+	}
+	$content['opengraph']['has_image'] = false;
+	if(isset($content['images']['shop']) && count($content['images']['shop'])) {
+		foreach($content['images']['shop'] as $og_img) {
+				$content['opengraph']['has_image'] = true;
+				set_meta('og:image', PHPWCMS_URL . 'img/cmsimage.php/500x500x0/'.$og_img['hash'].'.'.$og_img['ext'], 'property', false, true);
+		}
+	}
+	if(isset($content['images']['news']) && count($content['images']['news'])) {
+		foreach($content['images']['news'] as $og_img) {
+				$content['opengraph']['has_image'] = true;
+				set_meta('og:image', PHPWCMS_URL . 'img/cmsimage.php/500x500x0/'.$og_img['id'].'.'.$og_img['ext'], 'property', false, true);
+		}
+	}
+	if(isset($content['images']['article']['image'])) {
+		$content['opengraph']['has_image'] = true;
+		set_meta('og:image', PHPWCMS_URL . 'img/cmsimage.php/500x500x0/'.$content['images']['article']['hash'].'.'.$content['images']['article']['ext'], 'property');
+	}
+	if(!$content['opengraph']['has_image'] && is_file(PHPWCMS_TEMPLATE.'img/opengraph-default.png')) {
+		set_meta('og:image', PHPWCMS_URL.TEMPLATE_PATH.'img/opengraph-default.png', 'property');
+	}
+}
+
 set_meta('generator', 'phpwcms '.PHPWCMS_VERSION);
 
 // replace Print URL
