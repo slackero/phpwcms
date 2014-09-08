682a447a864714e79205d3c684272d66e50f9484
Oliver Georgi <slackero+github@gmail.com>
2014-09-08 12:31:52 +0200

    phpwcms v1.7.5

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,7 +9,7 @@
  *
  **/
 define('PHPWCMS_VERSION',		'1.7.5');
-define('PHPWCMS_RELEASE_DATE',	'2014/09/05');
+define('PHPWCMS_RELEASE_DATE',	'2014/09/07');
 define('PHPWCMS_REVISION',		'538');
 
 ?>
