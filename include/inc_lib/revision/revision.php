615e254b877cc26f6cb37579e96b90a73a296d9c
Oliver Georgi <slackero+github@gmail.com>
2015-01-16 19:10:48 +0100

    Updated jQuery to version 1.11.2 and 2.1.3

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,7 +9,7 @@
  *
  **/
 define('PHPWCMS_VERSION',		'1.7.6');
-define('PHPWCMS_RELEASE_DATE',	'2015/01/09');
+define('PHPWCMS_RELEASE_DATE',	'2015/01/16');
 define('PHPWCMS_REVISION',		'538');
 
 ?>
