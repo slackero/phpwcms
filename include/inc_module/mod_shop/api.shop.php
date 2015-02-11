697ee0f666a8d0565dc833305f430ce09312d6d0
Oliver Georgi <slackero+github@gmail.com>
2015-02-11 10:00:36 +0100

    Added shop module API information

---
+++ include/inc_module/mod_shop/api.shop.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Shop API for phpwcms Shop module
+ *
+ * @author Oliver Georgi <og@phpwcms.org>
+ *
+ **/
+
+?>
+<html>
+<body>
+	<h1>Shop API for phpwcms</h1>
+	<p>The Shop API for phpwcms is closed source and available on <strong>request only</strong>.</p>
+	<p>The API allows to get all order details as JSON object with simple GET/POST call. The response data can be restructured and parsed to feed a custom import script.</p>
+	<p>Contact me by <a href="mailto:og@phpwcms.org?subject=ShopAPI">email</a>.</p>
+</body>
+</html>
