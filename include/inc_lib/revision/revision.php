91305bda8e47ceea3612893816621328fb913432
Oliver Georgi <slackero+github@gmail.com>
2015-02-14 12:17:37 +0100

    On the way to v1.7.8

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,7 +9,7 @@
  *
  **/
 define('PHPWCMS_VERSION',		'1.7.7');
-define('PHPWCMS_RELEASE_DATE',	'2015/02/09');
+define('PHPWCMS_RELEASE_DATE',	'2015/02/14');
 define('PHPWCMS_REVISION',		'538');
 
 ?>
