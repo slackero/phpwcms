6c81c40801792c64f71aae854fc371b0f3159eda
Oliver Georgi <slackero+github@gmail.com>
2015-02-21 07:38:20 +0100

    Updated revision

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,7 +9,7 @@
  *
  **/
 define('PHPWCMS_VERSION',		'1.7.8');
-define('PHPWCMS_RELEASE_DATE',	'2015/02/19');
+define('PHPWCMS_RELEASE_DATE',	'2015/02/21');
 define('PHPWCMS_REVISION',		'538');
 
 ?>
