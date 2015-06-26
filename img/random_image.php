ea3c84b967ce00f0ccf5d90ac0704ea54e79a043
Oliver Georgi <slackero+github@gmail.com>
2015-06-26 17:30:57 +0200

    Full path to file was missing

--- img/random_image.php
+++ img/random_image.php
@@ -35,7 +35,7 @@ if($imgpath) {
 	if(is_dir($imgpath)) {
 		$handle = opendir( $imgpath );
 		while($file = readdir( $handle )) {
-   			if(substr($file, 0, 1) !== '.' && is_file($file) && preg_match('/(\.jpg|\.jpeg|\.png|\.gif)$/i', $file) )
+   			if(substr($file, 0, 1) !== '.' && is_file($imgpath.$file) && preg_match('/(\.jpg|\.jpeg|\.png|\.gif)$/i', $file) )
 				$imgArray[] = $file;
 		}
 		closedir( $handle );
