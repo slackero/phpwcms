6a677ee7cc902ffda6bdece1db69737a2a7346f6
Oliver Georgi <slackero+github@gmail.com>
2015-12-10 10:00:16 +0100

    Updated default VideoJS to v5.3

--- include/inc_front/js.inc.php
+++ include/inc_front/js.inc.php
@@ -133,7 +133,7 @@ function renderHeadJS($js) {
 function initVideoJs() {
 	if(empty($GLOBALS['phpwcms']['video-js'])) {
 		$GLOBALS['block']['custom_htmlhead']['video-js.ie8shim'] = '  <!--[if lt IE 9]><script src="'.PHPWCMS_HTTP_SCHEMA.'://vjs.zencdn.net/ie8/1.1.0/videojs-ie8.min.js"></script><![endif]-->';
-		$GLOBALS['phpwcms']['video-js'] = PHPWCMS_HTTP_SCHEMA.'://vjs.zencdn.net/5.2/';
+		$GLOBALS['phpwcms']['video-js'] = PHPWCMS_HTTP_SCHEMA.'://vjs.zencdn.net/5.3/';
 	} else {
 		$GLOBALS['phpwcms']['video-js'] = rtrim($GLOBALS['phpwcms']['video-js'], '/') . '/';
 	}
