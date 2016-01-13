2e3ae511250316ca7ac6c2e005c8fc3d37510a2e
Oliver Georgi <slackero+github@gmail.com>
2016-01-13 19:07:43 +0100

    Added jQuery v1.12.0 and v2.2.0

---
+++ include/inc_front/lib/js.jquery-1.12-migrate.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * phpwcms content management system
+ *
+ * @author Oliver Georgi <og@phpwcms.org>
+ * @copyright Copyright (c) 2002-2016, Oliver Georgi
+ * @license http://opensource.org/licenses/GPL-2.0 GNU GPL-2
+ * @link http://www.phpwcms.org
+ *
+ **/
+
+require_once(PHPWCMS_ROOT.'/include/inc_front/lib/js.jquery.default.php');
+
+define('PHPWCMS_JSLIB', 'jquery-1.12-migrate');
+
+/**
+ * Init jQuery 1.12.x + jQuery Migrate Library
+ */
+function initJSLib() {
+	if(empty($GLOBALS['block']['custom_htmlhead']['jquery.js'])) {
+		if(PHPWCMS_USE_CDN) {
+			// use jQuery CDN
+			$GLOBALS['block']['custom_htmlhead']['jquery.js'] = getJavaScriptSourceLink(PHPWCMS_HTTP_SCHEMA . '://code.jquery.com/jquery-1.12.0.min.js');
+			$GLOBALS['block']['custom_htmlhead']['jquery-migrate.js'] = getJavaScriptSourceLink(PHPWCMS_HTTP_SCHEMA . '://code.jquery.com/jquery-migrate-1.2.1.min.js');
+		} else {
+			$GLOBALS['block']['custom_htmlhead']['jquery.js'] = getJavaScriptSourceLink(TEMPLATE_PATH.'lib/jquery/jquery-1.12.0.min.js');
+			$GLOBALS['block']['custom_htmlhead']['jquery-migrate.js'] = getJavaScriptSourceLink(TEMPLATE_PATH.'lib/jquery/jquery-migrate-1.2.1.min.js');
+		}
+	}
+	return TRUE;
+}
+
+?>
