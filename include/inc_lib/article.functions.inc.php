55ba0022b2af7840ff0c86dc0979e3a0919a4462
geckse <geckse@users.noreply.github.com>
2016-04-17 16:45:44 +0200

    added features from module anchory (#166)
    - new Replacementtags: [LINK id/alias], [LINK
    id/alias]linktitle[/LINK], [LINKHREF id/alias]
    - these new RTs removing dead anchor-elements, content preserved.
    - ckeditor injection, which adds a article selection to the link dialog.

--- include/inc_lib/article.functions.inc.php
+++ include/inc_lib/article.functions.inc.php
@@ -122,4 +122,49 @@ function struct_select_list($counter=0, $struct_id=0, & $selected_id, $add_alias
 	}
 }
 
+/* -- get articles in a article-struct-form
+ *	@author: q23/geckse
+ */	
+function get_article_struct_option($counter=0, $struct_id=0, $prev = "") {
+
+	global $db;
+	
+	$output = ($prev != "" ? $prev : "");
+	$struct_id	= intval($struct_id);
+	$counter	= intval($counter) + 1;
+	
+	if($prev == "" && $struct_id == 0){
+		$sql="SELECT article_id, article_title, article_cid, article_sort FROM ".DB_PREPEND."phpwcms_article WHERE article_deleted='0' AND article_cid='0' ORDER BY article_sort ASC";
+		$indexresult = _dbQuery($sql);
+		foreach($indexresult as $article){
+			$output .= '<option value="[LINKHREF '.$article['article_id'].']">'.$article['article_title'].' [ID:'.$article['article_id'].']</option>'."\n";
+		}
+			
+	}		
+
+	$sql = "SELECT acat_id,acat_name,acat_alias FROM ".DB_PREPEND."phpwcms_articlecat WHERE acat_trash=0 AND acat_struct=".$struct_id." ORDER BY acat_sort;";
+	if($result = mysql_query($sql, $db)) {
+		$sx=0;
+		while($row = mysql_fetch_assoc($result)) {
+			$struct[$sx] = $row;
+			$sx++;
+		}
+		mysql_free_result($result);
+	}
+	if(isset($struct[0])) {
+		foreach($struct as $key => $value) {
+			
+			$sql="SELECT article_id, article_title, article_cid, article_sort FROM ".DB_PREPEND."phpwcms_article WHERE article_deleted='0' AND article_cid='".$struct[$key]["acat_id"]."' ORDER BY article_sort ASC";
+			$articleresult = _dbQuery($sql);
+			
+			foreach($articleresult as $article){
+				$output .= '<option value="'.$article['article_id'].'">'.str_repeat("&#8212;", $counter).' '.$article['article_title'].' [ID:'.$article['article_id'].']</option>'."\n";
+			}
+			$output = (get_article_struct_option($counter, $struct[$key]["acat_id"],$output));
+		}
+	}
+	return $output;
+}
+
+
 ?>
