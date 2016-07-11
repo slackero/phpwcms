582ec316dd5bf0329830c3d10e09f6d9cd475743
Chaffinch <bobi@poczta.fm>
2016-07-11 23:49:51 +0200

    Polish language

---
+++ include/inc_module/mod_calendar/lang/pl.lang.php
@@ -0,0 +1,64 @@
+<?php
+// Language: Polish, Language Code: pl UTF-8  Bogusław Zięba http://www.krynica.malopolska.pl
+// first define main language vars
+$BLM['backend_menu']	= 'Kalendarz/wydarzenie';
+
+
+$BLM['listing_title']	= 'Kalendarz';
+$BLM['create_new']	= 'Utwórz pozycję kalendarza';
+$BLM['calendar_entry']	= 'Pozycja kalendarza';
+$BLM['calendar_title']	= 'Co';
+$BLM['calendar_token']	= 'Kalendarz';
+$BLM['calendar_text']	= 'Opis';
+$BLM['calendar_teasertext'] = 'Tekst zapowiedzi';
+$BLM['delete_entry']	= 'Usuń pozycję kalendarza:';
+$BLM['input_filter']	= 'Filtr kalendarza';
+$BLM['listview']	= 'Podgląd listy';
+$BLM['max_words']	= 'Maks. ilość słów opisu';
+
+$BLM['alert_empty_title'] = 'Proszę wpisać wartości w polu "co".';
+
+$BLM['locale_string']	= 'pl_PL';
+$BLM['day_name_length']	= 2;
+$BLM['weekNrTitle']	= 'Tyd.';
+
+$BLM['today']		= 'Dzisiaj';
+$BLM['add_event']	= 'Dodaj nową datę/wydarzenie';
+$BLM['calendar_start']	= 'kiedy';
+$BLM['calendar_end']	= 'Do';
+$BLM['all_day']		= 'Wszystkie dni';
+$BLM['where']		= 'Gdzie';
+$BLM['repeat']		= 'Powtórzenia';
+$BLM['date_format']	= 'DD/MM/YYYY';
+$BLM['date_delimiter']	= '/';
+$BLM['time_format']	= 'HH:MM';
+
+$BLM['save_copy']	= 'Zapisz wpis jako duplikat';
+$BLM['delete']		= 'Usuń';
+$BLM['delete_entry']	= 'Następujący wpis kalendarza został usunięty:';
+
+
+$BLM['repeat_0']	= 'Nie powtarzaj';
+$BLM['repeat_1']	= 'Codziennie';
+$BLM['repeat_2']	= 'W dni powszednie (Pn-Pt)';
+$BLM['repeat_3']	= 'Każdy Pon., Śr. i Pt';
+$BLM['repeat_4']	= 'Każdy Wt i Czw';
+$BLM['repeat_5']	= 'Tygodniowo';
+$BLM['repeat_6']	= 'Miesięcznie';
+$BLM['repeat_7']	= 'Rocznie';
+
+$BLM['repeat_list1']	= 'Codziennie';
+$BLM['repeat_list2']	= 'Pon-Pią';
+$BLM['repeat_list3']	= 'Pon/Śro/Pt';
+$BLM['repeat_list4']	= 'Wto/Czw';
+$BLM['repeat_list5']	= 'Tygodniowo';
+$BLM['repeat_list6']	= 'Miesięcznie';
+$BLM['repeat_list7']	= 'Rocznie';
+
+$BLM['article_link']	= 'AID/alias/url';
+$BLM['more_info']	= '(wygeneruj dodatkowy link w w serwisie)'; 
+
+$BLM['repeat_till']	= 'Okres czasu'; 
+$BLM['till']		= 'Aż do';
+
+?>
