55baa2059b823b2668a593d6007082032542a957
Oliver Georgi <slackero+github@gmail.com>
2017-01-23 19:35:15 +0100

    Show IPTC information in file manager, option to set file info using IPTC values
    In addition there is a new config file `iptc.inc.php` which can be used to overwrite system default IPTC related settings and behaviour. Check the file `include/config/dist.iptc.inc.php`. To use it needs to be renamed to `iptc.inc.php`.

--- include/config/dist.iptc.inc.php
+++ include/config/dist.iptc.inc.php
@@ -10,24 +10,10 @@
  **/
 
 
-// ----------------------------------------------------------------
-// obligate check for phpwcms constants
-if (!defined('PHPWCMS_ROOT')) {
-	die("You Cannot Access This Script Directly, Have a Nice Day.");
-}
-// ----------------------------------------------------------------
-
-
-// user is admin
-define('IS_ADMIN',			empty($_SESSION["wcs_user_admin"]) ? false : true);
-define('BE_CURRENT_URL',	PHPWCMS_URL.'phpwcms.php?'.$_SERVER['QUERY_STRING']);
-define('ACTIVE_REFERER',	$_SESSION['REFERER_URL']);
-
-$_SESSION['REFERER_URL'] =	BE_CURRENT_URL;
-
-// some more important constants
-define('JS_START',	'<script type="text/javascript">' . LF . '<!--' . LF);
-define('JS_END',	LF . '// -->' . LF . '</script>');
+/**
+ * Configuration of how to handle IPTC fields and values
+ * Will overwrite system's defaults
+ */
 
 
 /**
@@ -37,48 +23,12 @@ define('JS_END',	LF . '// -->' . LF . '</script>');
  */
 $phpwcms['iptc_as_caption'] = false;
 
+
 /**
  * If set TRUE IPTC values will be used for alternative languages too
  */
-$phpwcms['iptc_as_caption_all_lang'] = false;
+$phpwcms['iptc_as_caption_all_lang'] = true;
 
-$phpwcms['iptc_keys'] = array(
-    'Artist' => '',
-    'CityDest' => '',
-    'Contact' => '',
-    'Copyright' => '',
-    'CountryCodeDest' => '',
-    'CountryDest' => '',
-    'Credit' => '',
-    'DateTimeDigitized' => '',
-    'DateTimeExpires' => '',
-    'DateTimeOriginal' => '',
-    'DateTimeReleased' => '',
-    'EditStatus' => '',
-    'FixtureIdentifier' => '',
-    'Headline' => '',
-    'ImageDescription' => '',
-    'IntellectualGenre' => '',
-    'Keywords' => '',
-    'LanguageCode' => '',
-    'LocationDest' => '',
-    'LocationDestCode' => '',
-    'ObjectCycle' => '',
-    'ObjectName' => '',
-    'OriginalTransmissionRef' => '',
-    'ProvinceOrStateDest' => '',
-    'Software' => '',
-    'SoftwareVersion' => '',
-    'Source' => '',
-    'SpecialInstructions' => '',
-    'SubjectNewsCode' => '',
-    'SublocationDest' => '',
-    'Urgency' => '',
-    'Writer' => '',
-    'iimCategory' => '',
-    'iimSupplementalCategory' => '',
-    'iimVersion' => '',
-);
 
 /**
  * Set rules
@@ -122,14 +72,13 @@ $phpwcms['iptc_keys'] = array(
  *
  * Use the fields like BBCode with ELSE tags as fallback
  */
-$phpwcms['iptc_default_rules'] = array(
+$phpwcms['iptc_rules'] = array(
     'title' => '[Headline]{Headline}[/Headline][Headline_ELSE][ObjectName]{ObjectName}[/ObjectName][/Headline_ELSE]',
     'longinfo' => '[ImageDescription]{ImageDescription}[Credit], {Credit}[/Credit][/ImageDescription][ImageDescription_ELSE][Credit]{Credit}[/Credit][/ImageDescription_ELSE]',
     'copyright' => '[Copyright]{Copyright}[/Copyright]',
     'alt' => '[ObjectName]{ObjectName}[/ObjectName]',
 );
 
-$phpwcms['iptc_rules'] = $phpwcms['iptc_default_rules'];
 
 /**
  * IPTC fields might contain multiple values (array),
@@ -140,6 +89,7 @@ $phpwcms['iptc_rules'] = $phpwcms['iptc_default_rules'];
  */
 $phpwcms['iptc_rules_multiple'] = false;
 
+
 /**
  * If multiple values should be used which text separator
  * should be used to join multiple values, like:
@@ -147,12 +97,3 @@ $phpwcms['iptc_rules_multiple'] = false;
  * - comma separated = ", "
  */
 $phpwcms['iptc_separator'] = ', ';
-
-// Load custom IPTC config
-if(is_file(PHPWCMS_ROOT.'/include/config/iptc.inc.php')) {
-
-    include_once PHPWCMS_ROOT.'/include/config/iptc.inc.php';
-
-    $phpwcms['iptc_rules'] = array_merge($phpwcms['iptc_default_rules'], $phpwcms['iptc_rules']);
-
-}
