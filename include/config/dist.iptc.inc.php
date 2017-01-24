09c27c1253ba4a91a1cab49ee845a47106b141a2
Oliver Georgi <slackero+github@gmail.com>
2017-01-24 09:07:49 +0100

    Add related IPTC tag to IPTC field comment

--- include/config/dist.iptc.inc.php
+++ include/config/dist.iptc.inc.php
@@ -34,41 +34,41 @@ $phpwcms['iptc_as_caption_all_lang'] = true;
  * Set rules
  *
  * IPTC fields, my be unset based on related image's IPTC settings:
- * - Artist
- * - CityDest
- * - Contact
- * - Copyright
- * - CountryCodeDest
- * - CountryDest
- * - Credit
- * - DateTimeDigitized
- * - DateTimeExpires
- * - DateTimeOriginal
- * - DateTimeReleased
- * - EditStatus
- * - FixtureIdentifier
- * - Headline
- * - ImageDescription
- * - IntellectualGenre
- * - Keywords
- * - LanguageCode
- * - LocationDest
- * - LocationDestCode
- * - ObjectCycle
- * - ObjectName
- * - OriginalTransmissionRef
- * - ProvinceOrStateDest
- * - Software
- * - SoftwareVersion
- * - Source
- * - SpecialInstructions
- * - SubjectNewsCode
- * - SublocationDest
- * - Urgency
- * - Writer
- * - iimCategory
- * - iimSupplementalCategory
- * - iimVersion
+ * - Artist (IPTC Tag 2#080)
+ * - CityDest (IPTC Tag 2#090)
+ * - Contact (IPTC Tag 2#118)
+ * - Copyright (IPTC Tag 2#120)
+ * - CountryCodeDest (IPTC Tag 2#100)
+ * - CountryDest (IPTC Tag 2#101)
+ * - Credit (IPTC Tag 2#110)
+ * - DateTimeDigitized (IPTC Tag 2#062)
+ * - DateTimeExpires (IPTC Tag 2#037)
+ * - DateTimeOriginal (IPTC Tag 2#055)
+ * - DateTimeReleased (IPTC Tag 2#030)
+ * - EditStatus (IPTC Tag 2#007)
+ * - FixtureIdentifier (IPTC Tag 2#022)
+ * - Headline (IPTC Tag 2#105)
+ * - ImageDescription (IPTC Tag 2#120)
+ * - IntellectualGenre (IPTC Tag 2#004)
+ * - Keywords (IPTC Tag 2#025)
+ * - LanguageCode (IPTC Tag 2#135)
+ * - LocationDest (IPTC Tag 2#027)
+ * - LocationDestCode (IPTC Tag 2#026)
+ * - ObjectCycle (IPTC Tag 2#075)
+ * - ObjectName (IPTC Tag 2#005)
+ * - OriginalTransmissionRef (IPTC Tag 2#103)
+ * - ProvinceOrStateDest (IPTC Tag 2#095)
+ * - Software (IPTC Tag 2#065)
+ * - SoftwareVersion (IPTC Tag 2#070)
+ * - Source (IPTC Tag 2#115)
+ * - SpecialInstructions (IPTC Tag 2#040)
+ * - SubjectNewsCode (IPTC Tag 2#012)
+ * - SublocationDest (IPTC Tag 2#092)
+ * - Urgency (IPTC Tag 2#010)
+ * - Writer (IPTC Tag 2#122)
+ * - iimCategory (IPTC Tag 2#015, deprecated)
+ * - iimSupplementalCategory (IPTC Tag 2#020, deprecated)
+ * - iimVersion (IPTC Tag 2#000, deprecated)
  *
  * Use the fields like BBCode with ELSE tags as fallback
  */
