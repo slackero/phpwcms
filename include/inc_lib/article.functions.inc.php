a8cbdb4986881f577888d63a44b1153963330c98
Oliver Georgi <slackero+github@gmail.com>
2017-03-07 14:29:47 +0100

    Implement function struct_radio_list(), some fixes

--- include/inc_lib/article.functions.inc.php
+++ include/inc_lib/article.functions.inc.php
@@ -91,7 +91,7 @@ function struct_select_list($counter=0, $struct_id=0, & $selected_id, $add_alias
 	$struct_id	= intval($struct_id);
 	$counter	= $counter + 1;
 
-	$struct = _dbGet('phpwcms_articlecat', 'acat_id,acat_name,acat_alias,acat_public,acat_regonly', 'acat_trash=0 AND acat_struct='.$struct_id);
+	$struct = _dbGet('phpwcms_articlecat', 'acat_id,acat_name,acat_alias,acat_aktiv,acat_regonly', 'acat_trash=0 AND acat_struct='.$struct_id);
 
 	if(isset($struct[0]['acat_id'])) {
 		foreach($struct as $key => $value) {
@@ -122,7 +122,7 @@ function struct_checkbox_list($counter=0, $struct_id=0, $selected_id=array(), $a
         $selected_id = is_null($selected_id) || is_bool($selected_id) ? array() : array(strval($selected_id));
     }
 
-	$struct = _dbGet('phpwcms_articlecat', 'acat_id,acat_name,acat_alias,acat_public,acat_regonly', 'acat_trash=0 AND acat_struct='.intval($struct_id));
+	$struct = _dbGet('phpwcms_articlecat', 'acat_id,acat_name,acat_alias,acat_aktiv,acat_regonly', 'acat_trash=0 AND acat_struct='.intval($struct_id));
 
 	if(isset($struct[0]['acat_id'])) {
 
@@ -140,8 +140,8 @@ function struct_checkbox_list($counter=0, $struct_id=0, $selected_id=array(), $a
 
 			$list .= '<li class="checkbox-list-item" id="checkbox-list-item-'.$value["acat_id"].'">';
 			$list .= '<label class="checkbox-label';
-			if(empty($value["acat_public"])) {
-    			$list .= ' checkbox-list-item-off';
+			if(empty($value["acat_aktiv"])) {
+    			$list .= ' disabled';
 			}
 			if(!empty($value["acat_regonly"])) {
     			$list .= ' checkbox-list-item-regonly';
@@ -169,4 +169,61 @@ function struct_checkbox_list($counter=0, $struct_id=0, $selected_id=array(), $a
 
 	return $list;
 }
+
+function struct_radio_list($counter=0, $struct_id=0, $selected_id=array(), $add_alias=false, $radio_name='structlist') {
+
+    $list = '';
+
+    if($counter === 0 && !is_array($selected_id)) {
+        $selected_id = is_null($selected_id) || is_bool($selected_id) ? array() : array(strval($selected_id));
+    }
+
+	$struct = _dbGet('phpwcms_articlecat', 'acat_id,acat_name,acat_alias,acat_aktiv,acat_regonly', 'acat_trash=0 AND acat_struct='.intval($struct_id));
+
+	if(isset($struct[0]['acat_id'])) {
+
+        $counter = $counter + 1;
+        $list .= '<ul class="radio-list radio-list-level-'.$counter.'">';
+
+		foreach($struct as $key => $value) {
+
+            $value['acat_name'] = html($value["acat_name"]);
+
+            if($value['acat_name'] === '' && $value["acat_alias"]) {
+                $value['acat_name'] = $value["acat_alias"];
+                $value["acat_alias"] = '';
+            }
+
+			$list .= '<li class="radio-list-item" id="radio-list-item-'.$value["acat_id"].'">';
+			$list .= '<label class="radio-label';
+			if(empty($value["acat_aktiv"])) {
+    			$list .= ' disabled';
+			}
+			if(!empty($value["acat_regonly"])) {
+    			$list .= ' radio-list-item-regonly';
+			}
+			$list .= '" title="'.$value['acat_name'].', ID: '.$value["acat_id"].'">';
+			$list .= '<input type="radio" value="'.$value["acat_id"].'" ';
+			$list .= 'name="'.$radio_name.'" id="radio-list-input-'.$value["acat_id"].'"';
+			if(in_array($value["acat_id"], $selected_id)) {
+				$list .= ' checked="checked"';
+			}
+			$list .= ' /> ' . $value['acat_name'];
+
+            if($add_alias && $value["acat_alias"]) {
+				$list .= ' <em class="radio-list-item-alias">'.$value["acat_alias"].'</em>';
+			}
+
+            $list .= '</label>';
+			$list .= struct_radio_list($counter, $value["acat_id"], $selected_id, $add_alias, $radio_name);
+			$list .= '</li>';
+
+		}
+
+		$list .= '</ul>';
+	}
+
+	return $list;
+}
+
 
