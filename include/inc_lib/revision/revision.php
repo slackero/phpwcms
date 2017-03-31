a600e7dbbcb0a67baab4db6049897c629ccf9764
Oliver Georgi <slackero+github@gmail.com>
2017-03-31 10:17:41 +0200

    Add missing closing slash
    # Conflicts:
    #	include/inc_lib/revision/revision.php

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -10,5 +10,5 @@
  **/
 
 define('PHPWCMS_VERSION', '1.9.0-beta.4');
-define('PHPWCMS_RELEASE_DATE', '2017/03/24');
+define('PHPWCMS_RELEASE_DATE', '2017/03/31');
 define('PHPWCMS_REVISION', '545');
