3e0c57c1a7f09f8e4a70d9842b2b43421b9f236c
Oliver Georgi <slackero+github@gmail.com>
2017-04-23 08:20:48 +0200

    Refactored newsletter subscription, no table layout any longer
    A bit optimised structure, sorry some hard coded classes and texts, but have a look at template/template_lang!

--- include/inc_lib/content/cnt12.readform.inc.php
+++ include/inc_lib/content/cnt12.readform.inc.php
@@ -12,30 +12,30 @@
 // ----------------------------------------------------------------
 // obligate check for phpwcms constants
 if (!defined('PHPWCMS_ROOT')) {
-	die("You Cannot Access This Script Directly, Have a Nice Day.");
+    die("You Cannot Access This Script Directly, Have a Nice Day.");
 }
 // ----------------------------------------------------------------
 
 
 // Content Type Newsletter Subscription
-$content["newsletter"]["text"] 					= html_specialchars(clean_slweg($_POST["cnewsletter_text"]));
-$content["newsletter"]["label_email"]			= html_specialchars(clean_slweg($_POST["cnewsletter_label_email"]));
-$content["newsletter"]["label_name"]			= html_specialchars(clean_slweg($_POST["cnewsletter_label_name"]));
-$content["newsletter"]["label_subscriptions"]	= html_specialchars(clean_slweg($_POST["cnewsletter_label_subscriptions"]));
-$content["newsletter"]["all_subscriptions"]		= html_specialchars(clean_slweg($_POST["cnewsletter_all_subscriptions"]));
-$content["newsletter"]["button_text"]			= html_specialchars(clean_slweg($_POST["cnewsletter_button_text"]));
-$content["newsletter"]["success_text"]			= html_specialchars(clean_slweg($_POST["cnewsletter_success_text"]));
-$content["newsletter"]["reg_text"]				= html_specialchars(clean_slweg($_POST["cnewsletter_reg_text"]));
-$content["newsletter"]["logoff_text"]			= html_specialchars(clean_slweg($_POST["cnewsletter_logoff_text"]));
-$content["newsletter"]["change_text"]			= html_specialchars(clean_slweg($_POST["cnewsletter_change_text"]));
-$content["newsletter"]["url1"]					= clean_slweg($_POST["cnewsletter_url1"]);
-$content["newsletter"]["url2"]					= clean_slweg($_POST["cnewsletter_url2"]);
+$content["newsletter"]["text"]                  = clean_slweg($_POST["cnewsletter_text"]);
+$content["newsletter"]["label_email"]           = clean_slweg($_POST["cnewsletter_label_email"]);
+$content["newsletter"]["label_name"]            = clean_slweg($_POST["cnewsletter_label_name"]);
+$content["newsletter"]["label_subscriptions"]   = clean_slweg($_POST["cnewsletter_label_subscriptions"]);
+$content["newsletter"]["all_subscriptions"]     = clean_slweg($_POST["cnewsletter_all_subscriptions"]);
+$content["newsletter"]["button_text"]           = clean_slweg($_POST["cnewsletter_button_text"]);
+$content["newsletter"]["success_text"]          = clean_slweg($_POST["cnewsletter_success_text"]);
+$content["newsletter"]["reg_text"]              = clean_slweg($_POST["cnewsletter_reg_text"]);
+$content["newsletter"]["logoff_text"]           = clean_slweg($_POST["cnewsletter_logoff_text"]);
+$content["newsletter"]["change_text"]           = clean_slweg($_POST["cnewsletter_change_text"]);
+$content["newsletter"]["url1"]                  = clean_slweg($_POST["cnewsletter_url1"]);
+$content["newsletter"]["url2"]                  = clean_slweg($_POST["cnewsletter_url2"]);
 
-$content['subscription_temp']					= convertStringToArray($_POST['cnewsletter_subscription_left']);
-$content["newsletter"]["subscription"]			= array();
+$content['subscription_temp']                   = convertStringToArray($_POST['cnewsletter_subscription_left']);
+$content["newsletter"]["subscription"]          = array();
 foreach($content['subscription_temp'] as $subscr_value) {
-	$subscr_value = intval($subscr_value);
-	$content["newsletter"]["subscription"][$subscr_value] = $subscr_value;
+    $subscr_value = intval($subscr_value);
+    $content["newsletter"]["subscription"][$subscr_value] = $subscr_value;
 }
 
-$content["newsletter"]["pos"]					= intval($_POST["cnewsletter_pos"]);
+$content["newsletter"]["pos"]                   = intval($_POST["cnewsletter_pos"]);
