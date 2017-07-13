82b478fcdc80abe16550af896799d0e9b4d4511d
slackero <slackero@gmail.com>
2017-07-13 14:54:43 +0200

    Fixed issue #217

--- include/inc_lib/content/cnt24.readform.inc.php
+++ include/inc_lib/content/cnt24.readform.inc.php
@@ -18,25 +18,25 @@ if (!defined('PHPWCMS_ROOT')) {
 // ----------------------------------------------------------------
 
 // Content Type Alias
-$content["alias"] = array();
-$content["alias"]['alias_ID']       = isset($_POST["calias"]) ? intval($_POST["calias"]) : '';
-$content["alias"]['alias_block']    = empty($_POST["cablock"]) ? 0 : 1;
-$content["alias"]['alias_spaces']   = empty($_POST["caspaces"]) ? 0 : 1;
-$content["alias"]['alias_title']    = empty($_POST["catitle"]) ? 0 : 1;
-$content["alias"]['alias_toplink']  = empty($_POST["catop"]) ? 0 : 1;
-$content["alias"]['alias_status']   = empty($_POST["castatus"]) ? 0 : 1;
+$content['alias'] = array(
+    'alias_ID' => isset($_POST["calias"]) ? intval($_POST["calias"]) : '',
+    'alias_block' => empty($_POST["cablock"]) ? 0 : 1,
+    'alias_spaces' => empty($_POST["caspaces"]) ? 0 : 1,
+    'alias_title' => empty($_POST["catitle"]) ? 0 : 1,
+    'alias_toplink' => empty($_POST["catop"]) ? 0 : 1,
+    'alias_status' => empty($_POST["castatus"]) ? 0 : 1
+);
 
-if(empty($content["alias"]['alias_ID'])) {
+if(empty($content['alias']['alias_ID'])) {
 
-    $content["alias"]['alias_ID'] = '';
+    $content['alias']['alias_ID'] = '';
 
 } else {
 
     // check if alias ID has valid counter part
-    $sql_cnt = "SELECT acontent_id FROM ".DB_PREPEND."phpwcms_articlecontent WHERE acontent_id=".$content["alias"]['alias_ID']." AND acontent_trash=0";
-    $cresult = _dbQuery($sql);
+    $cresult = _dbGet('phpwcms_articlecontent', 'acontent_id', 'acontent_id='.$content['alias']['alias_ID'].' AND acontent_trash=0');
     if(empty($cresult[0]['acontent_id'])) {
-        $content["alias"]['alias_ID'] = '';
+        $content['alias']['alias_ID'] = '';
     }
 
 }
