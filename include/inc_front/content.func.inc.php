bd30f99d87f6fc6cef1442596419e51e6d6d9032
slackero <slackero@gmail.com>
2017-12-29 16:39:24 +0100

    Updated Google Analytics code

--- include/inc_front/content.func.inc.php
+++ include/inc_front/content.func.inc.php
@@ -1448,15 +1448,14 @@ if(!empty($block['tracking_ga']['enable'])) {
 
     $template_default['settings']['tracking']['ga_default'] = array(
         'position' => 'head',
-        'code' => "  <script".SCRIPT_ATTRIBUTE_TYPE.">
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', '%s', 'auto'%s);
-    ga('send', 'pageview');
+        'code' => "  <script".SCRIPT_ATTRIBUTE_TYPE." src=\"https://www.googletagmanager.com/gtag/js?id=%1\$s\" async></script>
+  <script".SCRIPT_ATTRIBUTE_TYPE.">
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '%1\$s'%2\$s);
   </script>",
-       'anonymize' => ', {anonymizeIp: true}',
+       'anonymize' => ", {'anonymize_ip': true}",
        'optout' => "  <script".SCRIPT_ATTRIBUTE_TYPE.">
     var gaOptOutCookie = 'ga-disable-%s';
     if (document.cookie.indexOf(gaOptOutCookie + '=true') > -1) {
