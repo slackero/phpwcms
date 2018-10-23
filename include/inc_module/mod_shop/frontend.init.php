3da26a6cf8b1b9cef2a24d01ce5754aa2ff9cd03
slackero <slackero@gmail.com>
2018-10-23 07:42:56 +0300

    Optimise

--- include/inc_module/mod_shop/frontend.init.php
+++ include/inc_module/mod_shop/frontend.init.php
@@ -96,20 +96,19 @@ function shop_image_tag($img=array(), $counter=0, $title='') {
 		// now try to build caption and if neccessary add alt to image or set external link for image
 		$caption	= getImageCaption(array('caption' => $img['caption'], 'file' => $img['f_id']));
 		// set caption and ALT Image Text for imagelist
-		$capt_cur	= html($caption[0]);
-		$caption[3] = empty($caption[3]) ? '' : ' title="'.html($caption[3]).'"'; //title
+        $caption[3] = empty($caption[3]) ? $title : ' title="'.html($caption[3]).'"'; //title
 		$caption[1] = html(empty($caption[1]) ? $img['f_name'] : $caption[1]);
 
 		$list_img_temp  = '<img src="'.$thumb_image['src'].'" ';
-		$list_img_temp .= $thumb_image[3].' alt="'.$caption[1].'"'.$caption[3].$title.' />';
+		$list_img_temp .= $thumb_image[3].' alt="'.$caption[1].'"'.$caption[3].' />';
 
 		// use lightbox effect
 		if($config['image_'.$config['mode'].'_lightbox']) {
 
-			$a  = '<a href="'.PHPWCMS_RESIZE_IMAGE.'/';
+			$a  = '<a href="'.CMSGO_RESIZE_IMAGE.'/';
 			$a .= $config['image_zoom_width'] . 'x' . $config['image_zoom_height'] . '/';
 			$a .= $img['f_hash'] . '.' . $img['f_ext'] . '" ';
-            $a .= 'target="_blank" rel="lightbox'.$config['lightbox_id'].'"' . get_attr_data_gallery($config['lightbox_id'], ' ', '') . $caption[3] . $title . '>';
+            $a .= 'target="_blank" rel="lightbox'.$config['lightbox_id'].'"' . get_attr_data_gallery($config['lightbox_id'], ' ', '') . $caption[3] . '>';
 
 			$list_img_temp = $a . $list_img_temp . '</a>';
 		}
