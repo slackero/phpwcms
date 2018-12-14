14c1111f6f7ff39e732acac6c65bb992727ab8b0
slackero <slackero@gmail.com>
2018-12-14 20:28:43 +0300

    Bump version to 2.0.2

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -8,6 +8,6 @@
  *
  **/
 
-define('CMSGO_VERSION', '2.0.1');
-define('CMSGO_RELEASE_DATE', '2018/10/23');
+define('CMSGO_VERSION', '2.0.2');
+define('CMSGO_RELEASE_DATE', '2018/12/14');
 define('CMSGO_REVISION', '549');
