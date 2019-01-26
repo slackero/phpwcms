7956aa79ca16c045d27b3085aa96291e1169fa24
slackero <slackero@gmail.com>
2019-01-27 00:02:59 +0300

    Updated CKEditor v4.11.2, exif-js to v2.3.0

--- include/inc_js/tooltip.js
+++ include/inc_js/tooltip.js
@@ -1,17 +1,13 @@
 // Tooltips for phpwcms Backend
 window.addEvent('domready', function(){
-
     var as = [];
-
     $$('input').each(function(a){
         if(a.getAttribute('title')) {
             as.push(a);
         }
     });
-
     new Tips(as, {
         maxTitleChars: 25,
         offsets: {'x': 2, 'y': 5}
     });
-
 });
