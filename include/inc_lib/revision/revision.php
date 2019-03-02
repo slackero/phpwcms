cb1c7a5df7b2b2ee5e0e799a3664247480a05a99
slackero <slackero@gmail.com>
2019-03-02 12:47:16 +0300

    Release version 1.9.7-rc.3

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,6 +9,6 @@
  *
  **/
 
-define('PHPWCMS_VERSION', '1.9.7-rc.2');
-define('PHPWCMS_RELEASE_DATE', '2019/02/08');
+define('PHPWCMS_VERSION', '1.9.7-rc.3');
+define('PHPWCMS_RELEASE_DATE', '2019/01/02');
 define('PHPWCMS_REVISION', '549');
