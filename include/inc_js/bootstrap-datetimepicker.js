15103c8241a89221ea85117a03a031c1122cc068
slackero <slackero@gmail.com>
2020-01-15 00:01:06 +0300

    Updated dependencies
    – CKEditor version 4.13.1
    – ParseDown and ParseDown Extra
    – FontAwesome 5.12.0
    – Bootstrap 4.4.1
    – Fixed Bootstrap DateTimePicker

--- include/inc_js/bootstrap-datetimepicker.js
+++ include/inc_js/bootstrap-datetimepicker.js
@@ -2460,13 +2460,13 @@
         disabledDates: false,
         enabledDates: false,
         icons: {
-          time: 'fa fa-clock-o',
+          time: 'fa fa-clock',
           date: 'fa fa-calendar',
           up: 'fa fa-chevron-up',
           down: 'fa fa-chevron-down',
           previous: 'fa fa-chevron-left',
           next: 'fa fa-chevron-right',
-          today: 'glyphicon glyphicon-screenshot',
+          today: 'fa fa-dot-circle',
           clear: 'far fa-trash-alt',
           close: 'fa fa-times'
         },
--- include/inc_lib/backend.functions.inc.php
+++ include/inc_lib/backend.functions.inc.php
@@ -1023,7 +1023,7 @@ function initJsAutocompleter() {
 function initJQuery() {
 //  unset($GLOBALS['BE']['HEADER']['mootools.js']);
     // add jQuery at first position and keep the key
-    $GLOBALS['BE']['HEADER'] = array('jquery.js' => getJavaScriptSourceLink('include/inc_js/jquery/jquery-3.3.1.min.js')) + $GLOBALS['BE']['HEADER'];
+    $GLOBALS['BE']['HEADER'] = array('jquery.js' => getJavaScriptSourceLink('include/inc_js/jquery/jquery-3.4.1.min.js')) + $GLOBALS['BE']['HEADER'];
 }
 
 // make cmsgo compatibility and upgrade check
