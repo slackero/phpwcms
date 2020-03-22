1f598a218371693bb6e1b6feba73d2a6292fac99
slackero <slackero@gmail.com>
2020-03-22 18:49:11 +0400

    Fixes deprecated each() functions

--- include/inc_ext/htmlfilter.php
+++ include/inc_ext/htmlfilter.php
@@ -69,14 +69,12 @@ function tagprint($tagname, $attary, $tagtype){
         $fulltag = '</' . $tagname . '>';
     } else {
         $fulltag = '<' . $tagname;
-        if (is_array($attary) && sizeof($attary)){
-            $atts = array();
-            while (list($attname, $attvalue) = each($attary)){
-                array_push($atts, "$attname=$attvalue");
+        if (is_array($attary) && count($attary)) {
+            foreach ($attary as $attname => $attvalue) {
+                $fulltag .= ' ' . $attname . '=' . $attvalue;
             }
-            $fulltag .= ' ' . join(' ', $atts);
         }
-        if ($tagtype == 3){
+        if ($tagtype == 3) {
             $fulltag .= ' /';
         }
         $fulltag .= '>';
@@ -108,7 +106,7 @@ function casenormalize(&$val){
  */
 function skipspace($body, $offset){
     $me = 'skipspace';
-    if (preg_match('/^(\s*)/s', substr($body, $offset), $matches)) {;
+    if (preg_match('/^(\s*)/s', substr($body, $offset), $matches)) {
         $offset += strlen($matches[1]);
     }
     return $offset;
@@ -283,8 +281,7 @@ function getnxtag($body, $offset){
             //htmlfilter_debug("$me: Found invalid character '/'.\n");
             $gt = findnxstr($body, $pos, '>');
             //htmlfilter_debug("$me: Tag is invalid. Returning.\n");
-            $retary = array(false, false, false, $lt, $gt);
-            return $retary;
+            return array(false, false, false, $lt, $gt);
         }
     case '>':
         //htmlfilter_debug("$me: End of tag found at $pos\n");
@@ -608,48 +605,50 @@ function fixatts($tagname,
                  ){
     $me = 'fixatts';
     //htmlfilter_debug("$me: Fixing attributes\n");
-    while (list($attname, $attvalue) = each($attary)){
-        /**
-         * See if this attribute should be removed.
-         */
-        foreach ($rm_attnames as $matchtag=>$matchattrs){
-            if (preg_match($matchtag, $tagname)){
-                foreach ($matchattrs as $matchattr){
-                    if (preg_match($matchattr, $attname)){
-                        //htmlfilter_debug("$me: Attribute '$attname' defined as bad.\n");
-                        //htmlfilter_debug("$me: Removing.\n");
-                        unset($attary[$attname]);
-                        continue;
+    if (is_array($attary) && count($attary)) {
+        foreach ($attary as $attname => $attvalue) {
+            /**
+             * See if this attribute should be removed.
+             */
+            foreach ($rm_attnames as $matchtag=>$matchattrs){
+                if (preg_match($matchtag, $tagname)){
+                    foreach ($matchattrs as $matchattr){
+                        if (preg_match($matchattr, $attname)){
+                            //htmlfilter_debug("$me: Attribute '$attname' defined as bad.\n");
+                            //htmlfilter_debug("$me: Removing.\n");
+                            unset($attary[$attname]);
+                            continue;
+                        }
                     }
                 }
             }
-        }
-        /**
-         * Remove any backslashes, entities, or extraneous whitespace.
-         */
-        defang($attvalue);
-        unspace($attvalue);
+            /**
+             * Remove any backslashes, entities, or extraneous whitespace.
+             */
+            defang($attvalue);
+            unspace($attvalue);
 
-        /**
-         * Now let's run checks on the attvalues.
-         * I don't expect anyone to comprehend this. If you do,
-         * get in touch with me so I can drive to where you live and
-         * shake your hand personally. :)
-         */
-        foreach ($bad_attvals as $matchtag=>$matchattrs){
-            if (preg_match($matchtag, $tagname)){
-                foreach ($matchattrs as $matchattr=>$valary){
-                    if (preg_match($matchattr, $attname)){
-                        /**
-                         * There are two arrays in valary.
-                         * First is matches.
-                         * Second one is replacements
-                         */
-                        list($valmatch, $valrepl) = $valary;
-                        $newvalue = preg_replace($valmatch,$valrepl,$attvalue);
-                        if ($newvalue != $attvalue){
-                            //htmlfilter_debug("$me: attvalue is now $newvalue\n");
-                            $attary[$attname] = $newvalue;
+            /**
+             * Now let's run checks on the attvalues.
+             * I don't expect anyone to comprehend this. If you do,
+             * get in touch with me so I can drive to where you live and
+             * shake your hand personally. :)
+             */
+            foreach ($bad_attvals as $matchtag=>$matchattrs){
+                if (preg_match($matchtag, $tagname)){
+                    foreach ($matchattrs as $matchattr=>$valary){
+                        if (preg_match($matchattr, $attname)){
+                            /**
+                             * There are two arrays in valary.
+                             * First is matches.
+                             * Second one is replacements
+                             */
+                            list($valmatch, $valrepl) = $valary;
+                            $newvalue = preg_replace($valmatch,$valrepl,$attvalue);
+                            if ($newvalue != $attvalue){
+                                //htmlfilter_debug("$me: attvalue is now $newvalue\n");
+                                $attary[$attname] = $newvalue;
+                            }
                         }
                     }
                 }
