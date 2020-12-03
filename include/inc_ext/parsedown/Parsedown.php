f58ebd2df9336867be3ed82878f8199cecc5c7e7
slackero <slackero@gmail.com>
2020-12-03 09:31:07 +0100

    Updated dependencies
    – CKEditor 4.15.1
    – Bootstrap 4.5.3
    – FontAwesome 5.15.1
    – Moment.js 2.29.1
    – ParseDown 1.8.0-beta-7
    – ParseDown Extra 0.8.1
    – SimplePie 1.5.5

--- include/inc_ext/parsedown/Parsedown.php
+++ include/inc_ext/parsedown/Parsedown.php
@@ -107,6 +107,7 @@ class Parsedown
         'ftp://',
         'ftps://',
         'mailto:',
+        'tel:',
         'data:image/png;base64,',
         'data:image/gif;base64,',
         'data:image/jpeg;base64,',
@@ -555,7 +556,7 @@ class Parsedown
 
         $Block = array(
             'element' => array(
-                'name' => 'h' . min(6, $level),
+                'name' => 'h' . $level,
                 'handler' => array(
                     'function' => 'lineElements',
                     'argument' => $text,
@@ -1133,6 +1134,9 @@ class Parsedown
 
     protected function lineElements($text, $nonNestables = array())
     {
+        # standardize line breaks
+        $text = str_replace(array("\r\n", "\r"), "\n", $text);
+
         $Elements = array();
 
         $nonNestables = (empty($nonNestables)
@@ -1490,7 +1494,7 @@ class Parsedown
 
     protected function inlineSpecialCharacter($Excerpt)
     {
-        if ($Excerpt['text'][1] !== ' ' and strpos($Excerpt['text'], ';') !== false
+        if (substr($Excerpt['text'], 1, 1) !== ' ' and strpos($Excerpt['text'], ';') !== false
             and preg_match('/^&(#?+[0-9a-zA-Z]++);/', $Excerpt['text'], $matches)
         ) {
             return array(
