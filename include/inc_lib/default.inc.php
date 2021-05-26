42843e7bc18c8d43142eb32588510300a499aca5
slackero <slackero@gmail.com>
2021-05-26 12:39:50 +0300

    Fixes problem loading the home with parameters like ?fbclid=IwA…
    It could result in 404 error because the parameter key was interpreted as alias.

--- include/inc_lib/default.inc.php
+++ include/inc_lib/default.inc.php
@@ -698,7 +698,6 @@ function buildGlobalGET($return = '') {
 
     unset(
         $_GET[session_name()],
-        $GLOBALS['_getVar'][$_getVar_first],
         $GLOBALS['_getVar'][session_name()],
         $GLOBALS['_getVar']['']
     );
@@ -709,8 +708,11 @@ function buildGlobalGET($return = '') {
         }
     }
 
-    $_getVar_first = trim($_getVar_first, " \t\n\r\0\x0B/"); // cleanup alias
-    $GLOBALS['_getVar'] = array($_getVar_first => '') + $GLOBALS['_getVar'];
+    if ($GLOBALS['_getVar'][$_getVar_first] === '') {
+        unset($GLOBALS['_getVar'][$_getVar_first]);
+        $_getVar_first = trim($_getVar_first, " \t\n\r\0\x0B/"); // cleanup alias
+        $GLOBALS['_getVar'] = array($_getVar_first => '') + $GLOBALS['_getVar'];
+    }
 
     if(!IS_PHP7 && get_magic_quotes_gpc()) {
         foreach($GLOBALS['_getVar'] as $key => $value) {
