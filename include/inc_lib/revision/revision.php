ccd36d2b27764d48e5ebcfa05a5e76722980777a
slackero <slackero@gmail.com>
2021-10-15 09:07:45 +0300

    Try to reset backend cookies and re-init session after upgrade

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -10,5 +10,5 @@
  **/
 
 define('PHPWCMS_VERSION', '1.9.31-dev');
-define('PHPWCMS_RELEASE_DATE', '2021/10/14');
-define('PHPWCMS_REVISION', '552');
+define('PHPWCMS_RELEASE_DATE', '2021/10/15');
+define('PHPWCMS_REVISION', '553');
