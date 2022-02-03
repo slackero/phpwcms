e8264ae594bb2d619c0c70d20478c9e9cffeed71
slackero <slackero@gmail.com>
2022-02-03 10:28:20 +0300

    Fixes issue #325
    # Conflicts:
    #	include/inc_act/act_upload.php

--- include/inc_act/act_upload.php
+++ include/inc_act/act_upload.php
@@ -21,24 +21,24 @@ require CMSGO_ROOT.'/include/inc_lib/general.inc.php';
 require CMSGO_ROOT.'/include/inc_js/uploader/fileuploader.php';
 
 if(@ini_get('post_max_size')) {
-	$post_max_size = return_bytes(ini_get('post_max_size'));
-	if($post_max_size < $cmsgo['file_maxsize']) {
-		$cmsgo['file_maxsize'] = $post_max_size - 1;
-	}
+    $post_max_size = return_bytes(ini_get('post_max_size'));
+    if($post_max_size < $cmsgo['file_maxsize']) {
+        $cmsgo['file_maxsize'] = $post_max_size - 1;
+    }
 } else {
-	$post_max_size = $cmsgo['file_maxsize'];
+    $post_max_size = $cmsgo['file_maxsize'];
 }
 if(@ini_get('upload_max_filesize')) {
-	$upload_max_filesize = return_bytes(ini_get('upload_max_filesize'));
-	if($upload_max_filesize < $cmsgo['file_maxsize']) {
-		$cmsgo['file_maxsize'] = $upload_max_filesize - 1;
-	}
+    $upload_max_filesize = return_bytes(ini_get('upload_max_filesize'));
+    if($upload_max_filesize < $cmsgo['file_maxsize']) {
+        $cmsgo['file_maxsize'] = $upload_max_filesize - 1;
+    }
 } else {
-	$upload_max_filesize = $cmsgo['file_maxsize'];
+    $upload_max_filesize = $cmsgo['file_maxsize'];
 }
 
 if(is_string($cmsgo['allowed_upload_ext'])) {
-	$cmsgo['allowed_upload_ext'] = convertStringToArray(strtolower($cmsgo['allowed_upload_ext']));
+    $cmsgo['allowed_upload_ext'] = convertStringToArray(strtolower($cmsgo['allowed_upload_ext']));
 }
 
 $uploader	= new qqFileUploader($cmsgo['allowed_upload_ext'], min($post_max_size, $upload_max_filesize, $cmsgo['file_maxsize']));
@@ -51,36 +51,36 @@ $result['filename']	= $uploader->getUploadName();
 
 if(!empty($result['success']) && !empty($_GET['file_public'])) {
 
-	require_once CMSGO_ROOT.'/include/inc_lib/dbcon.inc.php';
+    require_once CMSGO_ROOT.'/include/inc_lib/dbcon.inc.php';
 
-	$data = array(
-		'f_pid'			=> intval($_GET['file_dir']),
-		'f_uid'			=> intval($_SESSION["wcs_user_id"]),
-		'f_kid'			=> 1,
-		'f_aktiv'		=> 1,
-		'f_public'		=> 1,
-		'f_name'		=> $result['filename'],
-		'f_created'		=> now(),
-		'f_size'		=> $uploader->getFileSize(),
-		'f_type'		=> $uploader->getFileType(),
-		'f_ext'			=> strtolower($uploader->getFileExtension()),
-		'f_svg'         => 0,
-		'f_longinfo'	=> slweg($_GET['file_longinfo']),
-		'f_hash'		=> md5( $result['filename'] . microtime() ),
-		'f_copyright'	=> slweg($_GET['file_copyright']),
-		'f_tags'		=> clean_slweg($_GET['file_tags'])
-	);
+    $data = array(
+        'f_pid'			=> intval($_GET['file_dir']),
+        'f_uid'			=> intval($_SESSION["wcs_user_id"]),
+        'f_kid'			=> 1,
+        'f_aktiv'		=> 1,
+        'f_public'		=> 1,
+        'f_name'		=> $result['filename'],
+        'f_created'		=> now(),
+        'f_size'		=> $uploader->getFileSize(),
+        'f_type'		=> $uploader->getFileType(),
+        'f_ext'			=> strtolower($uploader->getFileExtension()),
+        'f_svg'         => 0,
+        'f_longinfo'	=> slweg($_GET['file_longinfo']),
+        'f_hash'		=> md5( $result['filename'] . microtime() ),
+        'f_copyright'	=> slweg($_GET['file_copyright']),
+        'f_tags'		=> clean_slweg($_GET['file_tags'])
+    );
 
-	if(CMSGO_CHARSET != 'utf-8') {
-		$data['f_name']			= makeCharsetConversion($data['f_name'], 'utf-8', CMSGO_CHARSET);
-		$data['f_longinfo']		= makeCharsetConversion($data['f_longinfo'], 'utf-8', CMSGO_CHARSET);
-		$data['f_copyright']	= makeCharsetConversion($data['f_copyright'], 'utf-8', CMSGO_CHARSET);
-		$data['f_tags']			= makeCharsetConversion($data['f_tags'], 'utf-8', CMSGO_CHARSET);
-	}
+    if(CMSGO_CHARSET != 'utf-8') {
+        $data['f_name']			= makeCharsetConversion($data['f_name'], 'utf-8', CMSGO_CHARSET);
+        $data['f_longinfo']		= makeCharsetConversion($data['f_longinfo'], 'utf-8', CMSGO_CHARSET);
+        $data['f_copyright']	= makeCharsetConversion($data['f_copyright'], 'utf-8', CMSGO_CHARSET);
+        $data['f_tags']			= makeCharsetConversion($data['f_tags'], 'utf-8', CMSGO_CHARSET);
+    }
 
-	$userftppath = CMSGO_ROOT.$cmsgo["ftp_path"];
+    $userftppath = CMSGO_ROOT.$cmsgo["ftp_path"];
 
-	// Try to detect image data
+    // Try to detect image data
     if($file_image_size = getimagesize($userftppath.$result['filename'])) {
 
         $data['f_image_width'] = $file_image_size[0];
@@ -99,44 +99,44 @@ if(!empty($result['success']) && !empty($_GET['file_public'])) {
 
     }
 
-	$insert = _dbInsert('cmsgo_file', $data);
+    $insert = _dbInsert('cmsgo_file', $data);
 
-	// move uploaded file
-	if(!empty($insert['INSERT_ID'])) {
+    // move uploaded file
+    if(!empty($insert['INSERT_ID'])) {
 
-		$useruploadpath = CMSGO_ROOT.$cmsgo["file_path"];
-		$usernewfile	= $useruploadpath.$data['f_hash'];
+        $useruploadpath = CMSGO_ROOT.$cmsgo["file_path"];
+        $usernewfile	= $useruploadpath.$data['f_hash'];
 
-		if($data['f_ext']) {
-			$usernewfile .= '.'.$data['f_ext'];
-		}
+        if($data['f_ext']) {
+            $usernewfile .= '.'.$data['f_ext'];
+        }
 
-		$oldmask = umask(0);
+        $oldmask = umask(0);
 
-		if($dir = @opendir($useruploadpath) && @copy($userftppath.$result['filename'], $usernewfile)) {
+        if($dir = @opendir($useruploadpath) && @copy($userftppath.$result['filename'], $usernewfile)) {
 
-			@unlink($userftppath.$result['filename']);
+            @unlink($userftppath.$result['filename']);
 
-		} else {
+        } else {
 
-			require CMSGO_ROOT.'/include/inc_lang/backend/en/lang.inc.php';
-			$cust_lang = CMSGO_ROOT.'/include/inc_lang/backend/' . strtolower(substr($_SESSION["wcs_user_lang"], 0, 2)) . '/lang.inc.php';
-			if(is_file($cust_lang)) {
-				include $cust_lang;
-			}
+            require CMSGO_ROOT.'/include/inc_lang/backend/en/lang.inc.php';
+            $cust_lang = CMSGO_ROOT.'/include/inc_lang/backend/' . strtolower(substr($_SESSION["wcs_user_lang"], 0, 2)) . '/lang.inc.php';
+            if(is_file($cust_lang)) {
+                include $cust_lang;
+            }
 
-			$result['success'] = false;
-			$result['error'] = $BL['be_error_while_save'];
+            $result['success'] = false;
+            $result['error'] = $BL['be_error_while_save'];
 
-			_dbQuery('DELETE FROM '.DB_PREPEND.'cmsgo_file WHERE f_id='._dbEscape($insert['INSERT_ID']));
+            _dbQuery('DELETE FROM '.DB_PREPEND.'cmsgo_file WHERE f_id='._dbEscape($insert['INSERT_ID']));
 
-		}
+        }
 
-		if(!empty($dir) && !is_bool($dir)) {
-			@closedir($dir);
-		}
+        if(!empty($dir) && !is_bool($dir)) {
+            @closedir($dir);
+        }
 
-	}
+    }
 
 }
 
