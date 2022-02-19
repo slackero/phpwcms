ff41671df6c1c682441cf499a0dae3c69c211e86
slackero <slackero@gmail.com>
2022-02-19 23:56:06 +0300

    Initialize before if
    # Conflicts:
    #	include/vendor/composer/installed.php

--- include/inc_front/content/cnt31.article.inc.php
+++ include/inc_front/content/cnt31.article.inc.php
@@ -89,6 +89,8 @@ if($image['template']) {
         $image['fieldgroup'] =& $template_default['settings']['imagespecial_custom_fields'][ $image['fieldgroup'] ]['fields'];
     }
 
+    $total = 0;
+
     if(is_array($image['images']) && ($image['count'] = count($image['images']))) {
 
         // Start lightbox
@@ -135,7 +137,6 @@ if($image['template']) {
 
         $x      = 0;
         $col    = 0;
-        $total  = 0;
 
         foreach($image['images'] as $key => $value) {
 
--- include/vendor/composer/installed.php
+++ include/vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'reference' => 'e5829ea1dc87e395a318e4c594f1d6bbdbabeaf6',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => 'e08f7ccbd6a9de056218486f18506329aefac40f',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e08f7ccbd6a9de056218486f18506329aefac40f',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'reference' => 'e5829ea1dc87e395a318e4c594f1d6bbdbabeaf6',
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.14.1',
+            'version' => '0.14.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'reference' => '307b42066fb0b76b5119f5e1f0826e18fefabe95',
+            'dev_requirement' => false,
+        ),
     ),
-    'enshrined/svg-sanitize' => 
-    array (
-      'pretty_version' => '0.14.1',
-      'version' => '0.14.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '307b42066fb0b76b5119f5e1f0826e18fefabe95',
-    ),
-  ),
 );
