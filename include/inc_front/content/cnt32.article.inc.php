4a77f2f407b301c8712c966128579933ca66776f
slackero <slackero@gmail.com>
2022-02-20 10:52:00 +0300

    Avoid some more possible PHP warnings

--- include/inc_front/content/cnt32.article.inc.php
+++ include/inc_front/content/cnt32.article.inc.php
@@ -119,8 +119,11 @@ if($tabs['template']) {
                     if(!empty($custom_field_value['id'])) {
 
                         $IS_NEWS_CP = true;
+
                         if (!is_array($value)) {
                             $value = array();
+                        } elseif (!isset($value['cnt_object']) || !is_array($value['cnt_object'])) {
+                            $value['cnt_object'] = array();
                         }
 
                         $value['cnt_object']['cnt_files'] = array(
