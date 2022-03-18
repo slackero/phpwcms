b0d598141f81e449d31c5af307149e0cff020376
slackero <slackero@gmail.com>
2022-03-18 05:22:11 +0100

    Cleanup

--- include/inc_front/content/cnt31.article.inc.php
+++ include/inc_front/content/cnt31.article.inc.php
@@ -90,7 +90,7 @@ if($image['template']) {
         $image['fieldgroup'] =& $template_default['settings']['imagespecial_custom_fields'][ $image['fieldgroup'] ]['fields'];
     }
 
-    $total  = 0;
+    $total = 0;
 
     if(is_array($image['images']) && ($image['count'] = count($image['images']))) {
 
