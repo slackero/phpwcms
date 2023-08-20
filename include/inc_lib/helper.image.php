b37985b81490dcac2f8cdb0ba4c84c316abe67d5
slackero <slackero@gmail.com>
2023-08-20 11:20:13 +0200

    Animated WebP is unsupported in PHP, do not convert

--- include/inc_lib/helper.image.php
+++ include/inc_lib/helper.image.php
@@ -82,26 +82,27 @@ class Phpwcms_Image_lib {
 
     // Language strings
     var $lang = array(
-        'imglib_source_image_required'   => "You must specify a source image in your preferences.",
-        'imglib_gd_required'             => "The GD image library is required for this feature.",
-        'imglib_gd_required_for_props'   => "Your server must support the GD image library in order to determine the image properties.",
+        'imglib_source_image_required' => "You must specify a source image in your preferences.",
+        'imglib_gd_required' => "The GD image library is required for this feature.",
+        'imglib_gd_required_for_props' => "Your server must support the GD image library in order to determine the image properties.",
         'imglib_unsupported_imagecreate' => "Your server does not support the GD function required to process this type of image.",
-        'imglib_gif_not_supported'       => "GIF images are often not supported due to licensing restrictions. You may have to use JPG or PNG images instead.",
-        'imglib_jpg_not_supported'       => "JPG images are not supported.",
-        'imglib_png_not_supported'       => "PNG images are not supported.",
-        'imglib_webp_not_supported'      => "WebP images are not supported.",
-        'imglib_jpg_or_png_required'     => "The image resize protocol specified in your preferences only works with JPEG or PNG image types.",
-        'imglib_copy_error'              => "An error was encountered while attempting to replace the file. Please make sure your file directory is writable.",
-        'imglib_rotate_unsupported'      => "Image rotation does not appear to be supported by your server.",
-        'imglib_libpath_invalid'         => "The path to your image library is not correct.  Please set the correct path in your image preferences.",
-        'imglib_image_process_failed'    => "Image processing failed. Please verify that your server supports the chosen protocol and that the path to your image library is correct.",
+        'imglib_gif_not_supported' => "GIF images are often not supported due to licensing restrictions. You may have to use JPG or PNG images instead.",
+        'imglib_jpg_not_supported' => "JPG images are not supported.",
+        'imglib_png_not_supported' => "PNG images are not supported.",
+        'imglib_webp_not_supported' => "WebP images are not supported.",
+        'imglib_webp_animated_not_supported' => "Animated WebP images are not supported.",
+        'imglib_jpg_or_png_required' => "The image resize protocol specified in your preferences only works with JPEG or PNG image types.",
+        'imglib_copy_error' => "An error was encountered while attempting to replace the file. Please make sure your file directory is writable.",
+        'imglib_rotate_unsupported' => "Image rotation does not appear to be supported by your server.",
+        'imglib_libpath_invalid' => "The path to your image library is not correct.  Please set the correct path in your image preferences.",
+        'imglib_image_process_failed' => "Image processing failed. Please verify that your server supports the chosen protocol and that the path to your image library is correct.",
         'imglib_rotation_angle_required' => "An angle of rotation is required to rotate the image.",
-        'imglib_writing_failed_gif'      => "GIF image.",
-        'imglib_invalid_path'            => "The path to the image is not correct.",
-        'imglib_copy_failed'             => "The image copy routine failed.",
-        'imglib_missing_font'            => "Unable to find a font to use.",
-        'imglib_save_failed'             => "Unable to save the image. Please make sure the image and file directory are writable.",
-        'imglib_image_cannot_opened'     => 'Unable to open the image. This might happen if the image source is broken or the image is damaged.',
+        'imglib_writing_failed_gif' => "GIF image.",
+        'imglib_invalid_path' => "The path to the image is not correct.",
+        'imglib_copy_failed' => "The image copy routine failed.",
+        'imglib_missing_font' => "Unable to find a font to use.",
+        'imglib_save_failed' => "Unable to save the image. Please make sure the image and file directory are writable.",
+        'imglib_image_cannot_opened' => 'Unable to open the image. This might happen if the image source is broken or the image is damaged.',
     );
     var $lang_localized = false;        // set to TRUE if overwritten once
 
@@ -1148,6 +1149,12 @@ class Phpwcms_Image_lib {
             case IMAGETYPE_WEBP:
                 if (!function_exists('imagecreatefromwebp')) {
                     $this->set_error(array('imglib_unsupported_imagecreate', 'imglib_webp_not_supported'));
+                    return false;
+                }
+                // Animated WebP isn't supported yet, needs to be detected and rejected
+                $webp_type = file_get_contents($filename, false, null, 12, 4);
+                if ($webp_type && strtoupper($webp_type) === 'VP8X') {
+                    $this->set_error('imglib_webp_animated_not_supported');
                     return false;
                 }
                 $im = @imagecreatefromwebp($path);
