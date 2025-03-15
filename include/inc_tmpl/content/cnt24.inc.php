4c557ea46bccd8a5e593435a9ccce5f8f7f222f1
slackero <slackero@gmail.com>
2025-03-15 07:40:27 +0100

    Bump version to 2.4.10-dev, already 2025

--- include/inc_tmpl/content/cnt24.inc.php
+++ include/inc_tmpl/content/cnt24.inc.php
@@ -3,7 +3,7 @@
  * cmsGO!
  *
  * @author Pixels & Points GmbH <info@pixels-points.ch>
- * @copyright Copyright (c) 2002-2024, Pixels & Points GmbH
+ * @copyright Copyright (c) 2002-2025, Pixels & Points GmbH
  * @license https://www.pixels-points.ch/cmsgo-license.html Pixels & Points cmsGO! license
  **/
 
