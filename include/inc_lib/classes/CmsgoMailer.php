0f75f0ac2b8cdcf163b5c6791cf41c622e1bdbac
slackero <slackero@gmail.com>
2025-05-11 10:04:58 +0200

    Added debug and error messages, respect if exceptions should be thrown or not.

--- include/inc_lib/classes/CmsgoMailer.php
+++ include/inc_lib/classes/CmsgoMailer.php
@@ -27,9 +27,14 @@ class CmsgoMailer extends PHPMailer
     ];
 
     public function __construct($config = []) {
+
+        $this->edebug('Init PHPMailer');
         parent::__construct();
 
+        $this->edebug('Start to configure cmsgoMailer');
+
         if (empty($config['SMTP_MAILER'])) {
+            $this->edebug('Set Mailer to mail');
             $this->isMail();
         } else {
             $config['SMTP_MAILER'] = strtolower($config['SMTP_MAILER']);
@@ -43,8 +48,10 @@ class CmsgoMailer extends PHPMailer
                 $this->isMail();
             }
         }
+        $this->edebug('Mailer set to ' . $this->Mailer);
 
         $this->Host = $config['SMTP_HOST'];
+        $this->edebug('Host set to ' . $config['SMTP_HOST']);
 
         $config['SMTP_PORT'] = empty($config['SMTP_PORT']) ? 0 : (int)$config['SMTP_PORT'];
         if ($config['SMTP_PORT']) {
@@ -59,81 +66,116 @@ class CmsgoMailer extends PHPMailer
         if (!empty($config['SMTP_SECURE'])) {
             $config['SMTP_SECURE'] = strtolower($config['SMTP_SECURE']);
             if ($config['SMTP_SECURE'] === PHPMailer::ENCRYPTION_STARTTLS) {
+                $this->edebug('Set SMTPSecure to tls');
                 $this->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 if (empty($config['SMTP_PORT'])) {
+                    $this->edebug('Force Port to 587');
                     $this->Port = 587;
                 }
             } elseif ($config['SMTP_SECURE'] === PHPMailer::ENCRYPTION_SMTPS) {
+                $this->edebug('Set SMTPSecure to ssl');
                 $this->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
                 if (empty($config['SMTP_PORT'])) {
+                    $this->edebug('Force Port to 465');
                     $this->Port = 465;
                 }
             } else {
-                throw new RuntimeException(
-                    'Invalid SMTP_SECURE value (must be "tls" or "ssl")'
-                );
+                $errorMessage = 'Invalid SMTP_SECURE value (must be "tls" or "ssl")';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
         }
 
         if (!empty($config['SMTP_AUTH_TYPE'])) {
             $config['SMTP_AUTH_TYPE'] = strtoupper($config['SMTP_AUTH_TYPE']);
             if ($config['SMTP_AUTH_TYPE'] === 'NTLM') {
-                throw new RuntimeException('NTLM authentication is not supported any longer');
+                $errorMessage = 'NTLM authentication is not supported any longer';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
+            $this->edebug('Set AuthType to ' . $config['SMTP_AUTH_TYPE']);
             $this->AuthType = $config['SMTP_AUTH_TYPE'];
         }
 
+        $this->edebug('Set CharSet to ' . $config['charset']);
         $this->CharSet = $config['charset'];
 
         if (!empty($config['default_lang']) && strtolower($config['default_lang']) !== 'en') {
+            $this->edebug('Try to set language to ' . $config['default_lang']);
             $this->setLanguage($config['default_lang']);
         }
 
         if ($this->AuthType === 'XOAUTH2') {
             if (empty($config['SMTP_XOAUTH_PROVIDER'])) {
-                throw new RuntimeException(
-                    'SMTP_XOAUTH_PROVIDER is required for OAuth2 authentication'
-                );
+                $errorMessage = 'SMTP_XOAUTH_PROVIDER is required for OAuth2 authentication';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
             $config['SMTP_XOAUTH_PROVIDER'] = strtolower($config['SMTP_XOAUTH_PROVIDER']);
             $allowedProviders = implode(', ', array_keys(self::OAUTH_PROVIDERS));
             if (!isset(self::OAUTH_PROVIDERS[$config['SMTP_XOAUTH_PROVIDER']])) {
-                throw new RuntimeException(
-                    'Invalid SMTP_XOAUTH_PROVIDER value (must be one of: ' . $allowedProviders . ')'
-                );
+                $errorMessage = 'Invalid SMTP_XOAUTH_PROVIDER value (must be one of: ' . $allowedProviders . ')';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
             if (empty($config['SMTP_CLIENT_ID'])) {
-                throw new RuntimeException(
-                    'SMTP_CLIENT_ID is required for OAuth2 authentication'
-                );
+                $errorMessage = 'SMTP_CLIENT_ID is required for OAuth2 authentication';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
             if (empty($config['SMTP_CLIENT_SECRET'])) {
-                throw new RuntimeException(
-                    'SMTP_CLIENT_SECRET is required for OAuth2 authentication'
-                );
+                $errorMessage = 'SMTP_CLIENT_SECRET is required for OAuth2 authentication';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
             if (empty($config['SMTP_REFRESH_TOKEN'])) {
-                throw new RuntimeException(
-                    'SMTP_REFRESH_TOKEN is required for OAuth2 authentication'
-                );
+                $errorMessage = 'SMTP_REFRESH_TOKEN is required for OAuth2 authentication';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
             if (empty($config['SMTP_USER'])) {
-                throw new RuntimeException(
-                    'SMTP_USER is required for OAuth2 authentication'
-                );
+                $errorMessage = 'SMTP_USER is required for OAuth2 authentication';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
 
             $this->isSMTP();
             $this->SMTPAuth = true;
-            $this->AuthType = 'XOAUTH2';
             $this->Password = '';
 
             if ($config['SMTP_XOAUTH_PROVIDER'] === 'google') {
-
+                $this->edebug('XOAUTH2 provider is Google');
                 if (empty($config['SMTP_HOST']) || $config['SMTP_HOST'] === 'localhost') {
+                    $this->edebug('SMTP_HOST is empty or localhost, set Host to smtp.gmail.com');
                     $this->Host = 'smtp.gmail.com';
                 }
+                $this->edebug('Force Port to 465');
                 $this->Port = 465;
+                $this->edebug('Set SMTPSecure to ssl');
                 $this->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 
                 $provider = new Google(
@@ -142,23 +184,29 @@ class CmsgoMailer extends PHPMailer
                         'clientSecret' => $config['SMTP_CLIENT_SECRET'],
                     ]
                 );
-
             } elseif (
                 $config['SMTP_XOAUTH_PROVIDER'] === 'azure'
                 ||
                 $config['SMTP_XOAUTH_PROVIDER'] === 'microsoft'
             ) {
+                $this->edebug('XOAUTH2 provider is ' . self::OAUTH_PROVIDERS[$config['SMTP_XOAUTH_PROVIDER']]);
 
                 if (empty($config['SMTP_TENANT_ID'])) {
-                    throw new RuntimeException(
-                        'SMTP_TENANT_ID is required for Microsoft Azure OAuth2 authentication'
-                    );
+                    $errorMessage = 'SMTP_TENANT_ID is required for OAuth2 authentication';
+                    $this->setError($errorMessage);
+                    $this->edebug($errorMessage);
+                    if ($this->exceptions) {
+                        throw new RuntimeException($errorMessage);
+                    }
                 }
 
                 if (empty($config['SMTP_HOST']) || $config['SMTP_HOST'] === 'localhost') {
+                    $this->edebug('SMTP_HOST is empty or localhost, set Host to smtp.office365.com');
                     $this->Host = 'smtp.office365.com';
                 }
+                $this->edebug('Force Port to 465');
                 $this->Port = 587;
+                $this->edebug('Set SMTPSecure to tls');
                 $this->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 
                 $provider = new Azure(
@@ -169,22 +217,32 @@ class CmsgoMailer extends PHPMailer
                     ]
                 );
             } else {
-                throw new RuntimeException(
-                    'Invalid SMTP_XOAUTH_PROVIDER value (must be one of: ' . $allowedProviders . ')'
-                );
+                $provider = null;
+                $errorMessage = 'Invalid SMTP_XOAUTH_PROVIDER value (must be one of: ' . $allowedProviders . ')';
+                $this->setError($errorMessage);
+                $this->edebug($errorMessage);
+                if ($this->exceptions) {
+                    throw new RuntimeException($errorMessage);
+                }
             }
 
-            $this->setOAuth(
-                new OAuth(
-                    [
-                        'provider' => $provider,
-                        'clientId' => $config['SMTP_CLIENT_ID'],
-                        'clientSecret' => $config['SMTP_CLIENT_SECRET'],
-                        'refreshToken' => $config['SMTP_REFRESH_TOKEN'],
-                        'userName' => $config['SMTP_USER'],
-                    ]
-                )
-            );
+            if ($provider !== null) {
+                $this->edebug('Set OAuth');
+
+                $this->setOAuth(
+                    new OAuth(
+                        [
+                            'provider' => $provider,
+                            'clientId' => $config['SMTP_CLIENT_ID'],
+                            'clientSecret' => $config['SMTP_CLIENT_SECRET'],
+                            'refreshToken' => $config['SMTP_REFRESH_TOKEN'],
+                            'userName' => $config['SMTP_USER'],
+                        ]
+                    )
+                );
+            }
         }
+
+        $this->edebug('Init cmsgoMailer done');
     }
 }
