552e741bb77f900679b3f19a678e01ffed756aae
slackero <slackero@gmail.com>
2026-05-25 12:40:35 +0200

    fix(shop): prevent undefined key warnings
    Initialize default order data keys and safe-guard payment method translations to avoid PHP warnings on incomplete orders or missing translations. General review. Added es and fr, updated shop language files

--- include/inc_module/mod_shop/inc/listing.categories.inc.php
+++ include/inc_module/mod_shop/inc/listing.categories.inc.php
@@ -11,102 +11,107 @@
 // ----------------------------------------------------------------
 // obligate check for cmsGO! constants
 if (!defined('CMSGO_ROOT')) {
-    die("You Cannot Access This Script Directly, Have a Nice Day.");
+    die('You Cannot Access This Script Directly, Have a Nice Day.');
 }
 // ----------------------------------------------------------------
 
-$_entry['query']			= '';
+$_entry['query'] = '';
+$_entry['post_filter'] = $_SESSION['filter_shop_category_raw'] ?? '';
 
 // create pagination
-if(isset($_GET['c'])) {
-    $_SESSION['list_count'] = $_GET['c'] == 'all' ? '99999' : intval($_GET['c']);
+if (isset($_GET['c'])) {
+    $_SESSION['list_count'] = $_GET['c'] === 'all' ? '99999' : (int)$_GET['c'];
 }
-if(isset($_GET['page'])) {
-    $_SESSION['detail_page'] = intval($_GET['page']);
+if (isset($_GET['page'])) {
+    $_SESSION['detail_page'] = (int)$_GET['page'];
 }
 
 // set default values for paginating
-if(empty($_SESSION['list_count'])) {
+if (empty($_SESSION['list_count'])) {
     $_SESSION['list_count'] = 25;
 }
 
 // paginate and search form processing
-if(isset($_POST['do_pagination'])) {
+if (isset($_POST['do_pagination'])) {
 
-    $_SESSION['list_active']	= empty($_POST['showactive']) ? 0 : 1;
-    $_SESSION['list_inactive']	= empty($_POST['showinactive']) ? 0 : 1;
+    $_SESSION['list_active'] = empty($_POST['showactive']) ? 0 : 1;
+    $_SESSION['list_inactive'] = empty($_POST['showinactive']) ? 0 : 1;
 
-    $_SESSION['filter_shop_category'] = clean_slweg($_POST['filter']);
-    if(empty($_SESSION['filter_shop_category'])) {
-        unset($_SESSION['filter_shop_category']);
+    $_entry['post_filter'] = clean_slweg($_POST['filter']);
+    $_SESSION['filter_shop_category_raw'] = $_entry['post_filter'];
+    $_SESSION['filter_shop_category'] = $_entry['post_filter'];
+    if (empty($_SESSION['filter_shop_category'])) {
+        unset($_SESSION['filter_shop_category'], $_SESSION['filter_shop_category_raw']);
     } else {
         $_SESSION['filter_shop_category'] = convertStringToArray($_SESSION['filter_shop_category'], ' ');
         $_POST['filter'] = $_SESSION['filter_shop_category'];
     }
 
-    $_SESSION['detail_page'] = intval($_POST['page']);
-
+    $_SESSION['detail_page'] = (int)($_POST['page'] ?? 1);
 }
 
-if(empty($_SESSION['detail_page'])) {
+if (empty($_SESSION['detail_page'])) {
     $_SESSION['detail_page'] = 1;
 }
 
-$_entry['list_active']		= isset($_SESSION['list_active'])	? $_SESSION['list_active']		: 1;
-$_entry['list_inactive']	= isset($_SESSION['list_inactive'])	? $_SESSION['list_inactive']	: 1;
-
+$_entry['list_active'] = $_SESSION['list_active'] ?? 1;
+$_entry['list_inactive'] = (int)($_SESSION['list_inactive'] ?? 1);
 
 // set correct status query
-if($_entry['list_active'] != $_entry['list_inactive']) {
-
-    if(!$_entry['list_active']) {
+if ($_entry['list_active'] !== $_entry['list_inactive']) {
+    if (!$_entry['list_active']) {
         $_entry['query'] .= 'cat_status=0';
     }
-    if(!$_entry['list_inactive']) {
+    if (!$_entry['list_inactive']) {
         $_entry['query'] .= 'cat_status=1';
     }
-
 } else {
     $_entry['query'] .= 'cat_status!=9';
 }
 $_entry['query'] .= " AND cat_type='module_shop'";
 
-if(isset($_SESSION['filter_shop_category']) && is_array($_SESSION['filter_shop_category']) && count($_SESSION['filter_shop_category'])) {
+if (isset($_SESSION['filter_shop_category']) && is_array($_SESSION['filter_shop_category']) && count($_SESSION['filter_shop_category'])) {
 
     $_entry['filter_array'] = array();
 
-    foreach($_SESSION['filter_shop_category'] as $_entry['filter']) {
+    foreach ($_SESSION['filter_shop_category'] as $_entry['filter']) {
         //usr_name, usr_login, usr_email
-        $_entry['filter_array'][] = "cat_name LIKE '%".aporeplace($_entry['filter'])."%'";
-        $_entry['filter_array'][] = "cat_info LIKE '%".aporeplace($_entry['filter'])."%'";
+        $_entry['filter_array'][] = "cat_name LIKE '%" . aporeplace($_entry['filter']) . "%'";
+        $_entry['filter_array'][] = "cat_info LIKE '%" . aporeplace($_entry['filter']) . "%'";
     }
-    if(count($_entry['filter_array'])) {
+    if (count($_entry['filter_array'])) {
 
-        $_SESSION['filter_shop_category'] = ' AND ('.implode(' OR ', $_entry['filter_array']).')';
+        $_SESSION['filter_shop_category'] = ' AND (' . implode(' OR ', $_entry['filter_array']) . ')';
         $_entry['query'] .= $_SESSION['filter_shop_category'];
 
     }
 
-} elseif(isset($_SESSION['filter_shop_category']) && is_string($_SESSION['filter_shop_category'])) {
+} elseif (isset($_SESSION['filter_shop_category']) && is_string($_SESSION['filter_shop_category'])) {
 
     $_entry['query'] .= $_SESSION['filter_shop_category'];
 
 }
 
 // paginating values
-$_entry['count_total'] = _dbQuery('SELECT COUNT(cat_id) FROM '.DB_PREPEND.'cmsgo_categories WHERE '.$_entry['query'], 'COUNT');
+$_entry['count_total'] = _dbQuery('SELECT COUNT(cat_id) FROM ' . DB_PREPEND . 'cmsgo_categories WHERE ' . $_entry['query'], 'COUNT');
 $_entry['pages_total'] = ceil($_entry['count_total'] / $_SESSION['list_count']);
-if($_SESSION['detail_page'] > $_entry['pages_total']) {
+if ($_SESSION['detail_page'] > $_entry['pages_total']) {
     $_SESSION['detail_page'] = empty($_entry['pages_total']) ? 1 : $_entry['pages_total'];
 }
 ?>
 
 <div class="form-group mb-3 text-center text-sm-left">
-  <a class="btn btn-sm btn-blue" href="<?php echo shop_url(array('controller=cat', 'edit=0')) ?>" title="<?php echo $BLM['create_new'] ?>"><i class="fa fa-plus"></i> <span><?php echo $BLM['create_new'] ?></span></a>
+    <a class="btn btn-sm btn-blue"
+       href="<?php echo shop_url(['controller=cat', 'edit=0']) ?>"
+       title="<?php echo $BLM['create_new'] ?>"><i class="fa fa-plus"></i>
+        <span><?php echo $BLM['create_new'] ?></span>
+    </a>
 </div>
 
-<form action="<?php echo shop_url('controller=cat') ?>" method="post" name="paginate" id="paginate"><input type="hidden" name="do_pagination" value="1" />
-  <div class="form-row align-items-center">
+<form action="<?php
+echo shop_url('controller=cat') ?>" method="post" name="paginate" id="paginate">
+    <input type="hidden" name="do_pagination" value="1" />
+    <div class="form-row align-items-center">
         <div class="col-12 col-sm">
             <div class="input-group">
                 <div class="input-group-prepend">
@@ -114,7 +119,7 @@ if($_SESSION['detail_page'] > $_entry['pages_total']) {
                         <input name="showactive" id="showactive" type="checkbox" onclick="this.form.submit();"<?php is_checked(1, $_entry['list_active'], 1) ?> />
                     </div>
                     <div class="input-group-text bg-danger border-0">
-                         <input name="showinactive" id="showinactive" type="checkbox" onclick="this.form.submit();"<?php is_checked(1, $_entry['list_inactive'], 1) ?> />
+                        <input name="showinactive" id="showinactive" type="checkbox" onclick="this.form.submit();"<?php is_checked(1, $_entry['list_inactive'], 1) ?> />
                     </div>
                 </div>
                 <div class="input-group-append">
@@ -125,25 +130,39 @@ if($_SESSION['detail_page'] > $_entry['pages_total']) {
 
         <div class="col-12 col-sm-auto">
             <div class="input-group input-group-sm my-3 my-sm-0">
-                <input name="filter" id="filter" size="15" data-toggle="tooltip" title="Filtern" class="form-control form-control-sm" value="<?php echo html($news->filter) ?>" type="search">
-                    <span class="input-group-append">
-                        <input class="btn btn-sm btn-secondary" name="gofilter" value="Filter" type="submit">
-                    </span>
+                <input name="filter" id="filter" size="15" data-toggle="tooltip" title="Filtern" class="form-control form-control-sm" value="<?php
+                echo html($_entry['post_filter']) ?>" type="search">
+                <span class="input-group-append">
+                    <input class="btn btn-sm btn-secondary" name="gofilter" value="Filter" type="submit">
+                </span>
             </div>
         </div>
 
         <div class="col-12 col-sm-auto text-right">
             <select class="form-control form-control-sm custom-select">
-                <option selected><?php echo $BL['be_article_rendering'] ?></option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=5'">5</option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=10'">10</option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=25'">25</option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=50'">50</option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=100'">100</option>
-                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=all'"><?php echo $BL['be_ftptakeover_all'] ?></option>
+                <option selected><?php
+                    echo $BL['be_article_rendering'] ?></option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=5'">
+                    5
+                </option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=10'">
+                    10
+                </option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=25'">
+                    25
+                </option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=50'">
+                    50
+                </option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=100'">
+                    100
+                </option>
+                <option onClick="window.location = 'cmsgo.php?do=modules&amp;module=shop&amp;controller=cat&amp;c=all'">
+                    <?php echo $BL['be_ftptakeover_all'] ?>
+                </option>
             </select>
         </div>
-  </div>
+    </div>
 </form>
 <div class="table-responsive">
     <table class="table table-sm mb-0 mt-2">
@@ -151,59 +170,57 @@ if($_SESSION['detail_page'] > $_entry['pages_total']) {
         // loop listing available newsletters
         $row_count = 0;
 
-        $sql  = "SELECT C1.*, ";
+        $sql = 'SELECT C1.*, ';
         $sql .= "IFNULL(CONCAT(C2.cat_name, ' / ', C1.cat_name), C1.cat_name) AS category FROM ";
-        $sql .= DB_PREPEND.'cmsgo_categories C1 ';
-        $sql .= 'LEFT JOIN '.DB_PREPEND.'cmsgo_categories C2 ';
+        $sql .= DB_PREPEND . 'cmsgo_categories C1 ';
+        $sql .= 'LEFT JOIN ' . DB_PREPEND . 'cmsgo_categories C2 ';
         $sql .= 'ON C1.cat_pid=C2.cat_id ';
-        $sql .= 'WHERE '.str_replace('cat_', 'C1.cat_', $_entry['query']).' ';
+        $sql .= 'WHERE ' . str_replace('cat_', 'C1.cat_', $_entry['query']) . ' ';
         $sql .= 'ORDER BY C1.cat_sort DESC, C2.cat_sort DESC, category ASC ';
-        $sql .= 'LIMIT '.(($_SESSION['detail_page']-1) * $_SESSION['list_count']).','.$_SESSION['list_count'];
+        $sql .= 'LIMIT ' . (($_SESSION['detail_page'] - 1) * $_SESSION['list_count']) . ',' . $_SESSION['list_count'];
 
         $data = _dbQuery($sql);
 
-        $_controller_link =  shop_url('controller=cat');
+        $_controller_link = shop_url('controller=cat');
 
-        if(isset($data[0]['cat_id'])) {
+        if (isset($data[0]['cat_id'])) {
 
-            foreach($data as $row) {
+            foreach ($data as $row) {
 
                 echo '<tr';
-                if(!$row['cat_pid']) echo ' data-toggle="tooltip" data-html="true" title="' . $BL['be_admin_page_category'] . ' ID: <b>' .$row["cat_id"]. '</b><br />'.$BL['be_cnt_sorting'].': <b>'.$row["cat_sort"].'</b>)"';
-                echo '>'.LF;
+                if (!$row['cat_pid']) {
+                    echo ' data-toggle="tooltip" data-html="true" title="' . $BL['be_admin_page_category'] . ' ID: <b>' . $row['cat_id'] . '</b><br />' . $BL['be_cnt_sorting'] . ': <b>' . $row['cat_sort'] . '</b>)"';
+                }
+                echo '>' . LF;
 
                 echo '<td width="25" style="padding:2px 3px 2px 4px;">';
                 echo '<i class="fa fa-tag fa-fw text-';
                 echo $row['cat_pid'] ? 'muted' : 'blue';
-                echo '"></i></td>'.LF;
+                echo '"></i></td>' . LF;
 
                 echo '<td class="dir" width="85%">';
                 echo $row['cat_pid'] ? '&nbsp;&nbsp;&nbsp;&nbsp;' : '&nbsp;';
-                echo html_specialchars($row['category'])."</td>\n";
-
+                echo html_specialchars($row['category']) . "</td>\n";
                 echo '<td class="dir" width="3%" align="center">&nbsp;' . $row['cat_sort'] . '&nbsp;</td>';
-
-
                 echo '<td width="10%" class="text-right text-nowrap">';
+                echo '<a class="btn btn-sm btn-blue mr-1" href="' . $_controller_link . '&amp;edit=' . $row['cat_id'] . '">';
+                echo '<i class="fa fa-pencil-alt"></i></a>';
+                echo '<button id="abtnshop' . $row['cat_id'] . '" class="btn fa btn-sm visible ';
+                echo ((int)$row['cat_status'] === 0 ? 'btn-danger' : 'btn-success') . ' mr-1" data-id="' . $row['cat_id'];
+                echo '" data-type="shop" data-table="categories" data-field="cat_status" data-fieldid="cat_id" aria-disabled="true" data-toggle="tooltip" title="';
+                echo $BL['be_tooltip_visibility'] . '"></button>';
+                echo '<a class="btn btn-sm btn-danger mr-1" href="' . $_controller_link . '&amp;delete=' . $row['cat_id'];
+                echo '" title="delete: ' . html_specialchars($row['cat_name']) . '"';
+                echo ' onclick="return confirm(\'' . $BLM['delete_entry'] . js_singlequote($row['cat_name']) . '\');">';
+                echo '<i class="far fa-trash-alt"></i></a>';
 
-                    echo '<a class="btn btn-sm btn-blue mr-1" href="'.$_controller_link.'&amp;edit='.$row["cat_id"].'">';
-                    echo '<i class="fa fa-pencil-alt"></i></a>';
-
-                            echo '<button id="abtnshop'.$row['cat_id'].'" class="btn fa btn-sm visible '.($row["cat_status"]==0 ? "btn-danger" : "btn-success").' mr-1" data-id="'.$row['cat_id'].'" data-type="shop" data-table="categories" data-field="cat_status" data-fieldid="cat_id" aria-disabled="true" data-toggle="tooltip" title="'.$BL['be_tooltip_visibility'].'"></button>';
-
-                    echo '<a class="btn btn-sm btn-danger mr-1" href="'.$_controller_link.'&amp;delete='.$row["cat_id"];
-                    echo '" title="delete: '.html_specialchars($row['cat_name']).'"';
-                    echo ' onclick="return confirm(\''.$BLM['delete_entry'].js_singlequote($row['cat_name']).'\');">';
-                    echo '<i class="far fa-trash-alt"></i></a>';
-
-                echo '</td>'.LF;
-
-                echo '</tr>'.LF;
+                echo '</td>' . LF;
+                echo '</tr>' . LF;
 
                 $row_count++;
             }
         } else {
-            echo '<tr><td colspan="4" class="tdtop5">'.$BL['be_empty_search_result'].'</td></tr>';
+            echo '<tr><td colspan="4" class="tdtop5">' . $BL['be_empty_search_result'] . '</td></tr>';
         }
         ?>
     </table>
---
+++ include/inc_module/mod_shop/lang/fr.lang.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * cmsGO!
+ *
+ * @author Pixels & Points GmbH <info@pixels-points.ch>
+ * @copyright Copyright (c) 2002-2026, Pixels & Points GmbH
+ * @license https://www.pixels-points.ch/cmsgo-license.html Pixels & Points cmsGO! license
+ *
+ **/
+
+// first define main language vars
+$BLM['backend_menu']            = 'Boutique/Produits';
+$BLM['listing_title']           = 'Boutique/Produits';
+$BLM['tab_default']             = 'Résumé de la boutique';
+$BLM['tab_products']            = 'Produits';
+$BLM['tab_categories']          = 'Catégories';
+$BLM['tab_orders']              = 'Commandes';
+$BLM['tab_preferences']         = 'Préférences';
+
+$BLM['create_new']              = 'Ajouter une catégorie';
+$BLM['create_new_prod']         = 'Ajouter un produit';
+$BLM['shop_category']           = 'Catégorie de produit boutique';
+$BLM['shop_product']            = 'Produit de la boutique';
+$BLM['prod_cat']                = 'Catégorie de produit';
+
+$BLM['cat_edit']                = 'Modifier la catégorie de produit boutique';
+$BLM['prod_edit']               = 'Modifier le produit boutique';
+$BLM['pref_edit']               = 'Modifier les préférences';
+$BLM['delete_entry']            = 'La catégorie sélectionnée sera supprimée !\nToutes les sous-catégories seront également supprimées !\nSupprimer la catégorie : ';
+$BLM['delete_product']          = 'Supprimer le produit ?\n';
+$BLM['delete_order']            = 'Supprimer la commande ?\n';
+
+$BLM['th_ordnr']                = 'N° com.';
+$BLM['th_modnr']                = 'N° mod.';
+$BLM['th_product']              = 'produit';
+$BLM['th_price']                = 'prix';
+
+$BLM['th_customer']             = 'client';
+$BLM['th_net']                  = 'total&nbsp;net';
+$BLM['th_gross']                = 'total&nbsp;brut';
+$BLM['th_payment']              = 'payer&nbsp;par';
+$BLM['th_date']                 = 'date';
+
+$BLM['dec_point']               = ',';
+$BLM['thousands_sep']           = ' ';
+
+$BLM['shopprod_description0']   = 'Description courte';
+$BLM['shopprod_description1']   = 'Info produit 1';
+$BLM['shopprod_description2']   = 'Info produit 2';
+$BLM['shopprod_description3']   = 'Info produit 3';
+$BLM['shopprod_price']          = 'Prix';
+$BLM['shopprod_name1']          = 'Produit';
+$BLM['shopprod_name2']          = 'Additionnel';
+$BLM['shopprod_model']          = 'Modèle/Type';
+$BLM['shopprod_ordernumber']    = 'Numéro de commande';
+
+$BLM['shopprod_netgross']       = 'Brut';
+$BLM['shopprod_net']            = 'Net';
+$BLM['shopprod_netgross_info']  = 'Tarification : Net (décoché) = hors taxes, Brut (coché) = TTC';
+$BLM['shopprod_vat']            = 'TVA';
+
+$BLM['shopprod_tag']            = 'Tag (*,*)';
+
+$BLM['shopprod_weight']         = 'Poids';
+$BLM['shopprod_weight_max']     = 'max.';
+$BLM['shopprod_units_weight']   = 'kg, oz, g';
+$BLM['shopprod_unit']           = 'Unité';
+$BLM['shopprod_currency']       = 'Devise';
+$BLM['shopprod_cart']           = 'Panier';
+$BLM['shopprod_max_cartitem']   = 'Max. produits/panier';
+$BLM['shopprod_vat_rates']      = 'Taux de TVA';
+
+$BLM['shopprod_size']           = 'p.ex. taille&nbsp;&nbsp;<br />titre&nbsp;&nbsp;<br />descr|+/-prix|#suffixe';
+$BLM['shopprod_color']          = 'p.ex. couleur';
+
+$BLM['shopprod_url']            = 'Lien du produit';
+
+$BLM['shopprod_email_to']       = 'Email nouvelle commande (À)';
+$BLM['shopprod_email_from']     = 'Expéditeur email (De)';
+
+$BLM['shopprod_shipping']       = 'Frais de port';
+$BLM['shopprod_payment_method'] = 'Méthode de paiement';
+$BLM['shopprod_payby_paypal']   = 'PayPal';
+$BLM['shopprod_payby_prepay']   = 'Paiement d\'avance';
+$BLM['shopprod_payby_pod']      = 'Paiement à la livraison';
+$BLM['shopprod_payby_onbill']   = 'Facture';
+$BLM['shopprod_payby_ccard']    = 'Carte de crédit';
+$BLM['shopprod_payby_cash']     = 'Espèces';
+
+$BLM['shopprod_email_paypal']   = 'Email PayPal';
+$BLM['shopprod_supported_ccard']= 'supporté';
+
+$BLM['shopprod_terms']          = 'Conditions générales de vente';
+$BLM['shop_order']              = 'Commande';
+
+$BLM['shopprod_order_subject']  = 'Votre commande';
+$BLM['shopprod_subcategory']    = 'Sous-catégorie&nbsp;de';
+$BLM['shopprod_is_main_cat']    = 'Catégorie principale';
+$BLM['shopprod_listall']        = 'Lister sous &quot;Tous les produits&quot;';
+
+$BLM['shopprod_id_shop']        = 'Cible de boutique';
+$BLM['shopprod_id_cart']        = 'Cible de panier';
+
+$BLM['shopprod_order']          = 'Détails de la commande';
+$BLM['shopprod_order_date']     = 'Date de la commande';
+$BLM['shopprod_order_address']  = 'Adresse';
+$BLM['shopprod_order_region']   = 'Région';
+$BLM['shopprod_custom_field']   = 'Champ personnalisé';
+$BLM['shopprod_date_long']      = 'd/m/Y, H:i:s';
+
+$BLM['shopprod_total_net']      = 'Total net';
+$BLM['shopprod_total_vat']      = 'TVA';
+$BLM['shopprod_total_gross']    = 'Total brut';
+
+$BLM['shopprod_subtotal']       = 'Sous-total';
+
+$BLM['shopprod_email_customer'] = 'Mail au client';
+$BLM['shopprod_email_shop']     = 'Mail à la boutique';
+$BLM['shopprod_ordered']        = 'Produits commandés';
+$BLM['shopprod_quantity']       = 'Quantité';
+$BLM['shopprod_total']          = 'Total';
+$BLM['shopprod_status_paid']    = 'Payé';
+$BLM['shopprod_status_sent']    = 'Envoyé/Retiré';
+$BLM['shopprod_status_done']    = 'Terminé';
+$BLM['shopprod_status_back']    = 'Retour';
+$BLM['shopprod_status_msg']     = 'Le statut de la commande a été modifié.';
+
+$BLM['shopprod_loworder']           = 'Quantité minimale';
+$BLM['shopprod_loworder_under']     = 'au-dessous de';
+$BLM['shopprod_loworder_charge']    = 'supplément';
+
+$BLM['shopprod_discount']           = 'Remise';
+$BLM['shopprod_discount_from']      = 'à partir du montant net';
+$BLM['shopprod_freeshipping']       = 'livraison gratuite';
+$BLM['shopprod_selfpickup']         = 'Retrait sur place';
+$BLM['shopprod_allowed']            = 'autorisé';
+$BLM['shopprod_is_selfpickup']      = 'Oui';
+$BLM['shopprod_isnot_selfpickup']   = 'Non';
+
+$BLM['shopprod_lang_support']   = 'Support des langues frontend';
+$BLM['shopprod_overwrite_meta'] = 'Le détail du produit remplace les métadonnées';
+
+$BLM['shopprod_distance'] = 'Distance (km)';
+$BLM['shopprod_distance_base'] = 'Adresse de départ';
+$BLM['shopprod_api'] = 'API';
+$BLM['shopprod_api_access'] = 'autoriser l\'accès';
+$BLM['shopprod_api_key'] = 'Clé API';
+
+$BLM['shopprod_inventory'] = 'Stock';
+$BLM['shopprod_autosubtract_off'] = 'NE PAS déduire le stock lors de la commande';
+
+//NEW entries PPMO
+$BLM['order_edit']              = 'Modifier la commande';
+
+$BLM['shopprod_on_request'] = 'Commande sur demande';
+$BLM['shopprod_on_request_button'] = 'URL de demande';
+$BLM['shopprod_on_request_url'] = 'https://www.example.com?title={PRODUCT}&article={NUM}';
