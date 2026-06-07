1c0dec3017bc09edcfe68a6c5e81cc85253f26b5
slackero <slackero@gmail.com>
2026-06-07 19:13:13 +0200

    bump: Release v1.12.1-dev and modernize README.md

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -9,6 +9,6 @@
  *
  **/
 
-const PHPWCMS_VERSION = '1.11.2-dev';
-const PHPWCMS_RELEASE_DATE = '2026/01/21';
+const PHPWCMS_VERSION = '1.12.1-dev';
+const PHPWCMS_RELEASE_DATE = '2026/06/07';
 const PHPWCMS_REVISION = '554';
