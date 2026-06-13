405171570548e1a54d64a372f7e811b1f2d06291
slackero <slackero@gmail.com>
2026-06-13 10:34:50 +0200

    chore(revision): bump version and release date
    Update development version to 2.5.1-dev and adjust release date to current.

--- include/inc_lib/revision/revision.php
+++ include/inc_lib/revision/revision.php
@@ -8,6 +8,6 @@
  *
  **/
 
-const CMSGO_VERSION = '2.5.0-dev';
-const CMSGO_RELEASE_DATE = '2026/06/12';
+const CMSGO_VERSION = '2.5.1-dev';
+const CMSGO_RELEASE_DATE = '2026/06/13';
 const CMSGO_REVISION = '556';
