57c90411b053d7d42aac2b7b55e12305540cd66c
slackero <slackero@gmail.com>
2026-06-17 06:56:40 +0200

    style(ads): modernize UI and Bootstrap markup for ads module
    - Update table, form control, and grid layouts to align with Bootstrap styles.
    - Replace legacy markup with updated card/container classes.
    - Update localization strings for German, English, and Polish translation files.

--- include/inc_module/mod_ads/inc/listing.adplace.inc.php
+++ include/inc_module/mod_ads/inc/listing.adplace.inc.php
@@ -17,70 +17,62 @@ if (!defined('CMSGO_ROOT')) {
 
 
 ?>
-<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="adplaces" class="ads">
+	<div class="card-body">
+		<div class="table-responsive">
+			<table class="table table-sm table-striped table-hover mb-0">
+				<thead>
+					<tr>
+						<th style="width: 40px;" class="text-center">&nbsp;</th>
+						<th><?php echo $BLM['adplace'] ?></th>
+						<th><?php echo $BLM['ad_format'] ?></th>
+						<th>RT</th>
+						<th><?php echo $BLM['ad_wxh'] ?></th>
+						<th style="width: 120px;" class="text-right">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+				$row_count = 0;
 
-	<tr>
-		<th width="25">&nbsp;</th>
-		<th><?php echo $BLM['adplace'] ?></th>
-		<th class="listFormat nowrap" nowrap="nowrap"><?php echo $BLM['ad_format'] ?></th>
-		<th class="listFormat">RT</th>
-		<th class="listFormat nowrap" nowrap="nowrap"><?php echo $BLM['ad_wxh'] ?></th>
-		<th>&nbsp;</th>
-	</tr>
+				$sql  = 'SELECT * FROM '.DB_PREPEND.'cmsgo_ads_place ap ';
+				$sql .= 'LEFT JOIN '.DB_PREPEND.'cmsgo_ads_formats af ON ';
+				$sql .=	'ap.adplace_format=af.adformat_id ';
+				$sql .= 'WHERE adplace_status!=9';
+				$data = _dbQuery($sql);
 
-<?php
-// loop listing available newsletters
-$row_count = 0;
+				$sql  = 'SELECT COUNT(*) FROM '.DB_PREPEND.'cmsgo_ads_campaign WHERE adcampaign_status!=9 AND adcampaign_place=';
 
-$sql  = 'SELECT * FROM '.DB_PREPEND.'cmsgo_ads_place ap ';
-$sql .= 'LEFT JOIN '.DB_PREPEND.'cmsgo_ads_formats af ON ';
-$sql .=	'ap.adplace_format=af.adformat_id ';
-$sql .= 'WHERE adplace_status!=9';
-//$sql .= 'LIMIT '.(($_SESSION['ads_page']-1) * $_SESSION['list_user_count']).','.$_SESSION['list_user_count'];
-$data = _dbQuery($sql);
-
-$sql  = 'SELECT COUNT(*) FROM '.DB_PREPEND.'cmsgo_ads_campaign WHERE adcampaign_status!=9 AND adcampaign_place=';
-
-foreach($data as $row) {
-
-	echo '<tr'.( ($row_count % 2) ? ' class="adsAltRow"' : '' ).'>'.LF;
-	echo '	<td width="25" style="padding:2px 3px 2px 4px;"><img src="img/famfamfam/layout.gif" alt="'.$BLM['adplace'].'" /></td>'.LF;
-
-	echo '	<td width="50%">'.html($row["adplace_title"])."</td>\n";
-
-	echo '	<td class="listFormat nowrap"">'.html($row["adformat_title"])."</td>\n";
-	echo '	<td class="listFormat">{ADS_'.$row["adplace_id"]."}</td>\n";
-	echo '	<td class="listFormat nowrap">'.$row["adplace_width"].'x'.$row["adplace_height"]."&nbsp;</td>\n";
-
-
-	echo '	<td align="right" nowrap="nowrap" class="button_td nowrap">';
-
-	echo '<a href="'.MODULE_HREF.'&amp;adplace=1&amp;edit='.$row["adplace_id"].'">';
-	echo '<img src="img/button/edit_22x13.gif" border="0" alt="" /></a>';
-
-	echo '<a href="'.MODULE_HREF.'&amp;adplace=1&amp;editid='.$row["adplace_id"].'&amp;verify=';
-	echo (($row["adplace_status"]) ? '0' : '1').'">';
-	echo '<img src="img/button/aktiv_12x13_'.$row["adplace_status"].'.gif" border="0" alt="" /></a>';
-
-	// check if campaign for place is available - then it's not possible t delete place
-	if(_dbQuery($sql.$row['adplace_id'], 'COUNT')) {
-		echo '<img src="img/button/trash_13x13_1.gif" border="0" alt="" class="inactive" />';
-	} else {
-		echo '<a href="'.MODULE_HREF.'&amp;adplace=1&amp;delete='.$row["adplace_id"];
-		echo '" title="delete: '.html($row["adplace_title"]).'"';
-		echo ' onclick="return confirm(\''.$BLM['delete_adplace'].js_singlequote($row["adplace_title"]).'\');">';
-		echo '<img src="img/button/trash_13x13_1.gif" border="0" alt="" /></a>';
-	}
-
-	echo "</td>\n</tr>\n";
-
-	$row_count++;
-}
-
-
-if($row_count) {
-	echo '<tr><td colspan="6" bgcolor="#92A1AF"><img src="img/leer.gif" alt="" width="1" height="1"></td></tr>';
-}
-
-?>
-</table>
+				foreach($data as $row) {
+					echo '<tr>';
+					echo '<td class="text-center"><i class="fas fa-th-large text-muted"></i></td>';
+					echo '<td>' . html($row["adplace_title"]) . '</td>';
+					$_format_key = 'format_' . strtolower(str_replace(array(' ', '-'), '_', $row["adformat_title"]));
+					$_format_title = isset($BLM[$_format_key]) ? $BLM[$_format_key] : $row["adformat_title"];
+					echo '<td>' . html($_format_title) . '</td>';
+					echo '<td>{ADS_' . $row["adplace_id"] . '}</td>';
+					echo '<td>' . $row["adplace_width"] . 'x' . $row["adplace_height"] . '</td>';
+					echo '<td class="text-right">';
+					
+					echo '<a href="' . MODULE_HREF . '&amp;adplace=1&amp;edit=' . $row["adplace_id"] . '" class="btn btn-sm btn-blue mr-1" title="Edit"><i class="fas fa-edit fa-fw"></i></a>';
+					
+					echo '<a href="' . MODULE_HREF . '&amp;adplace=1&amp;editid=' . $row["adplace_id"] . '&amp;verify=' . (($row["adplace_status"]) ? '0' : '1') . '" class="btn btn-sm ' . (($row["adplace_status"]) ? 'btn-success' : 'btn-secondary') . ' mr-1" title="Toggle Status">';
+					echo '<i class="fas ' . (($row["adplace_status"]) ? 'fa-eye' : 'fa-eye-slash') . ' fa-fw"></i></a>';
+					
+					if(_dbQuery($sql.$row['adplace_id'], 'COUNT')) {
+						echo '<button class="btn btn-sm btn-danger" disabled title="Delete"><i class="fas fa-trash fa-fw"></i></button>';
+					} else {
+						echo '<a href="' . MODULE_HREF . '&amp;adplace=1&amp;delete=' . $row["adplace_id"] . '" class="btn btn-sm btn-danger" title="Delete"';
+						echo ' onclick="return confirm(\'' . $BLM['delete_adplace'] . js_singlequote($row["adplace_title"]) . '\');">';
+						echo '<i class="fas fa-trash fa-fw"></i></a>';
+					}
+					
+					echo '</td>';
+					echo '</tr>';
+					$row_count++;
+				}
+				?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
