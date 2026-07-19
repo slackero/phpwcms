3b8c8bc65a6b3a974d1a2adb01f76b46489555b1
slackero <slackero@gmail.com>
2026-07-19 10:55:37 +0200

    fix(security): prevent template stored XSS in verify.php by escaping subscriber email address (migrated from cmsgo)

--- verify.php
+++ verify.php
@@ -94,7 +94,7 @@ if (!empty($_GET['s']) || !empty($_GET['u'])) {
 
 // some replacements
 $page = replaceGlobalRT($page);
-$page = str_replace('{EMAIL}', $email, $page);
+$page = str_replace('{EMAIL}', html($email), $page);
 
 // send non caching page header
 headerAvoidPageCaching();
