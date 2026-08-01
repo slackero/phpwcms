33ea6575e448a415e98196b2ee5cd05c2003edd3
slackero <slackero@gmail.com>
2026-08-01 13:05:01 +0200

    docs(about): update dependency copyright and license information
    Update extension and dependency copyright details displayed in the backend About page.
    
    - Add Bootstrap, Day.js, Dropzone, exif-js, Flag Icons, Flatpickr, Font Awesome Free, PHP Calendar, Tom Select, and Video.js.
    - Update copyright holder and license links for jQuery, IDNA Convert, Textile Parser, and Solmetra FormValidator (SPAF).
    - Remove obsolete entries for dynCalendar, Slimbox, and reCAPTCHA.

--- include/inc_tmpl/about.tmpl.php
+++ include/inc_tmpl/about.tmpl.php
@@ -58,6 +58,11 @@ if (!defined('PHPWCMS_ROOT')) {
   <p><strong><?php echo $BL['be_extensions_copyright']; ?></strong></p>
   <ul>
     <li>
+        <a href="https://getbootstrap.com/" target="_blank"><strong>Bootstrap</strong></a>,
+        Copyright &copy; Twitter / The Bootstrap Authors,
+        <a href="https://github.com/twbs/bootstrap/blob/main/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
         <a href="http://www.ckeditor.com" target="_blank"><strong>CKEditor</strong></a>,
         Copyright &copy; 2003-2017 Frederico Caldeira Knabben,
         <a href="http://ckeditor.com/terms-of-use#licenses" target="_blank">License</a>
@@ -65,7 +70,7 @@ if (!defined('PHPWCMS_ROOT')) {
     <li>
         <a href="https://commonmark.thephpleague.com/" target="_blank"><strong>CommonMark</strong></a>,
         Copyright &copy; Colin O'Dell,
-        <a href="https://github.com/thephpleague/commonmark/blob/main/LICENSE" target="_blank">BSD 3-Clause &quot;New&quot; or &quot;Revised&quot; License</a>
+        <a href="https://github.com/thephpleague/commonmark/blob/main/LICENSE" target="_blank">BSD 3-Clause License</a>
     </li>
     <li>
         <a href="http://mikolajj.republika.pl" target="_blank"><strong>ConvertCharset</strong></a>,
@@ -73,12 +78,23 @@ if (!defined('PHPWCMS_ROOT')) {
     </li>
     <li>
         <a href="https://www.mediawiki.org" target="_blank"><strong>ConvertibleTimestamp &amp; IPTC</strong></a>,
-        Copyright &copy; MediaWiki.org
+        Copyright &copy; MediaWiki.org,
         <a href="http://www.fsf.org/licensing/licenses/gpl.html" target="_blank">GNU GPL</a>
     </li>
     <li>
-        <strong>dynCalendar.js</strong>,
-        Copyright &copy; 2001, 2002 Richard Heyes
+        <a href="https://day.js.org/" target="_blank"><strong>Day.js</strong></a>,
+        Copyright &copy; IAMMULI,
+        <a href="https://github.com/iamkun/dayjs/blob/dev/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="https://www.dropzone.dev/" target="_blank"><strong>Dropzone</strong></a>,
+        Copyright &copy; Matias Meno,
+        <a href="https://github.com/dropzone/dropzone/blob/main/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="https://github.com/exif-js/exif-js" target="_blank"><strong>exif-js</strong></a>,
+        Copyright &copy; Jacob Seidelin,
+        <a href="https://github.com/exif-js/exif-js/blob/master/LICENSE" target="_blank">MIT License</a>
     </li>
     <li>
         <a href="http://www.famfamfam.com/" target="_blank"><strong>FamFamFam</strong></a>,
@@ -89,6 +105,21 @@ if (!defined('PHPWCMS_ROOT')) {
         <a href="http://www.bitfolge.de" target="_blank"><strong>FeedCreator</strong></a>,
         originally &copy; Kai Blankenhorn,
         <a href="http://www.fsf.org/licensing/licenses/lgpl.html" target="_blank">GNU LGPL</a>
+    </li>
+    <li>
+        <a href="https://flagicons.lipis.dev/" target="_blank"><strong>Flag Icons</strong></a>,
+        Copyright &copy; Panayiotis Lipiridis,
+        <a href="https://github.com/lipis/flag-icons/blob/main/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="https://flatpickr.js.org/" target="_blank"><strong>Flatpickr</strong></a>,
+        Copyright &copy; Gregory Pfister,
+        <a href="https://github.com/flatpickr/flatpickr/blob/master/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="https://fontawesome.com" target="_blank"><strong>Font Awesome Free</strong></a>,
+        Copyright &copy; Fonticons, Inc.,
+        <a href="https://fontawesome.com/license/free" target="_blank">CC BY 4.0 / SIL OFL 1.1 / MIT License</a>
     </li>
     <li>
         <a href="https://htmlpurifier.org/" target="_blank"><strong>HTML Purifier</strong></a>,
@@ -103,11 +134,11 @@ if (!defined('PHPWCMS_ROOT')) {
     <li>
         <a href="http://linux.duke.edu/projects/mini/htmlfilter/" target="_blank"><strong>Htmlfilter</strong></a>,
         Copyright &copy; 2002-2005 Duke University/<a href="http://www.mricon.com/">Konstantin Riabitsev</a>,
-        <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank"> GNU LGPL</a>
+        <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">GNU LGPL</a>
     </li>
     <li>
-        <a href="http://idnaconv.phlymail.de" target="_blank"><strong>IDNA Convert</strong></a>
-        Copyright &copy; 2004-2011 phlyLabs Berlin,
+        <a href="https://github.com/algo26-matthias/idna-convert" target="_blank"><strong>IDNA Convert</strong></a>,
+        Copyright &copy; Matthias Fischer / phlyLabs,
         <a href="http://www.fsf.org/licensing/licenses/lgpl.html" target="_blank">GNU LGPL</a>
     </li>
     <li>
@@ -115,16 +146,18 @@ if (!defined('PHPWCMS_ROOT')) {
         Matt Kruse &lt;matt@mattkruse.com&gt;
     </li>
     <li>
-        <a href="http://jquery.com/" target="_blank"><strong>jQuery</strong></a>,
-        Copyright &copy; 2011 John Resig,
-        <a href="http://www.opensource.org/licenses/mit-license.php">MIT License</a>,
-        <a href="http://www.fsf.org/licensing/licenses/gpl.html" target="_blank">GNU GPL</a>
+        <a href="https://jquery.com/" target="_blank"><strong>jQuery</strong></a>,
+        Copyright &copy; OpenJS Foundation and jQuery contributors,
+        <a href="https://jquery.org/license/" target="_blank">MIT License</a>
     </li>
-
     <li>
         <a href="https://github.com/thephpleague/oauth2-client" target="_blank"><strong>OAuth2 Client &amp; Providers (Google, Azure)</strong></a>,
         Copyright &copy; The League of Extraordinary Packages / greew,
         <a href="https://github.com/thephpleague/oauth2-client/blob/master/LICENSE" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="http://keithdevens.com/software/php_calendar" target="_blank"><strong>PHP Calendar</strong></a>,
+        Copyright &copy; Keith Devens
     </li>
     <li>
         <a href="https://github.com/PHPMailer/PHPMailer" target="_blank"><strong>PHPMailer</strong></a>,
@@ -136,12 +169,7 @@ if (!defined('PHPWCMS_ROOT')) {
         <a href="https://github.com/PHPOffice/PhpSpreadsheet/blob/master/LICENSE" target="_blank">MIT License</a>
     </li>
     <li>
-        <a href="https://developers.google.com/recaptcha/docs/php" target="_blank"><strong>reCAPTCHA</strong></a>
-        Copyright &copy; 2007 Mike Crawford, Ben Maurer,
-        <a href="http://www.opensource.org/licenses/mit-license.php" target="_blank">MIT License</a>
-    </li>
-    <li>
-        <a href="http://code.iamcal.com/php/rfc822/" target="_blank"><strong>RFC 822/2822/5322 Email Parser</strong></a>
+        <a href="http://code.iamcal.com/php/rfc822/" target="_blank"><strong>RFC 822/2822/5322 Email Parser</strong></a>,
         Copyright &copy; Cal Henderson,
         <a href="http://creativecommons.org/licenses/by/2.5/" target="_blank">CC-Attribution</a>,
         <a href="http://www.gnu.org/copyleft/gpl.html" target="_blank">GNU GPL3</a>
@@ -152,13 +180,8 @@ if (!defined('PHPWCMS_ROOT')) {
         <a href="http://www.opensource.org/licenses/bsd-license.php" target="_blank">BSD License</a>
     </li>
     <li>
-        <a href="http://www.digitalia.be/software/slimbox" target="_blank"><strong>Slimbox</strong></a>,
-        Copyright &copy; Christophe Beyls,
-        <a href="http://www.opensource.org/licenses/mit-license.php">MIT License</a>
-    </li>
-    <li>
-        <a href="http://www.solmetra.com" target="_blank"><strong>Solmetra FormValidator</strong></a>,
-        Copyright &copy; UAB Solmetra,
+        <a href="http://www.solmetra.com" target="_blank"><strong>Solmetra FormValidator (SPAF)</strong></a>,
+        Copyright &copy; Martynas / UAB Solmetra,
         <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0" target="_blank">GNU GPL</a>
     </li>
     <li>
@@ -177,14 +200,24 @@ if (!defined('PHPWCMS_ROOT')) {
         <a href="https://github.com/symfony/polyfill/blob/main/LICENSE" target="_blank">MIT License</a>
     </li>
     <li>
-        <a href="http://textpattern.googlecode.com/svn/development/4.x/textpattern/lib/classTextile.php" target="_blank"><strong>Textile Parser</strong></a>,
-        Copyright &copy; 2003-2004, Dean Allen,
+        <a href="https://github.com/textile/php-textile" target="_blank"><strong>Textile Parser</strong></a>,
+        Copyright &copy; Netcarver, Dean Allen,
         <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0" target="_blank">GNU GPL</a>
     </li>
     <li>
         <a href="https://www.tiny.cloud/" target="_blank"><strong>TinyMCE</strong></a>,
         Copyright &copy; Tiny Technologies, Inc.,
         <a href="https://github.com/tinymce/tinymce/blob/master/LICENSE.md" target="_blank">MIT License</a>
+    </li>
+    <li>
+        <a href="https://tom-select.js.org/" target="_blank"><strong>Tom Select</strong></a>,
+        Copyright &copy; Brian Reavis / Orchid Software,
+        <a href="https://github.com/tom-select/tom-select/blob/master/LICENSE" target="_blank">Apache-2.0 License</a>
+    </li>
+    <li>
+        <a href="https://videojs.com/" target="_blank"><strong>Video.js</strong></a>,
+        Copyright &copy; Brightcove, Inc.,
+        <a href="https://github.com/videojs/video.js/blob/main/LICENSE" target="_blank">Apache-2.0 License</a>
     </li>
     <li>
         <a href="http://www.walterzorn.com/" target="_blank"><strong>wz_js Scripts</strong></a>,
