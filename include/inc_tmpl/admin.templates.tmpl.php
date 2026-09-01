5d0018cef62409c5b40a44251a7322dd6031b410
slackero <slackero@gmail.com>
2026-09-01 23:33:21 +0200

    fix(backend): wrap piwik tracking inputs in column divs for proper grid alignment

--- include/inc_tmpl/admin.templates.tmpl.php
+++ include/inc_tmpl/admin.templates.tmpl.php
@@ -861,9 +861,13 @@ if(!isset($_GET["s"])) {
 
                                 <div id="piwik-tracking" class="form-group row g-2 align-items-center mt-1"<?php if (!$template['tracking_piwik']['enable']): ?> style="display:none;"<?php endif; ?>>
                                     <label class="col-sm-2 col-form-label text-end" for="template_piwik_id"><?php echo $BL['be_site_id']; ?></label>
-                                    <input type="text" name="template_piwik_id" class="form-control form-control-sm col-sm-2" placeholder="1" id="template_piwik_id" value="<?php echo empty($template['tracking_piwik']['id']) ? '' : $template['tracking_piwik']['id']; ?>"/>
+                                    <div class="col-sm-2">
+                                        <input type="text" name="template_piwik_id" class="form-control form-control-sm" placeholder="1" id="template_piwik_id" value="<?php echo empty($template['tracking_piwik']['id']) ? '' : $template['tracking_piwik']['id']; ?>"/>
+                                    </div>
                                     <label class="col-sm-2 col-form-label text-end" for="template_piwik_url"><?php echo $BL['be_piwik_url']; ?></label>
-                                    <input type="text" name="template_piwik_url" class="form-control form-control-sm col-sm-4" placeholder="piwik.example.com" id="template_piwik_url" value="<?php echo html($template['tracking_piwik']['url']) ?>"/>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="template_piwik_url" class="form-control form-control-sm" placeholder="piwik.example.com" id="template_piwik_url" value="<?php echo html($template['tracking_piwik']['url']) ?>"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
